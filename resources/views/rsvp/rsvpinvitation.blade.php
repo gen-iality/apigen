747e1e448c82efa387fc3ea6583f54e1c0d37193
Felipe Martinez <felipe.martinez@mocionsoft.com>
2018-07-18 16:32:17 -0500

    Estilos Correo

--- resources/views/rsvp/rsvpinvitation.blade.php
+++ resources/views/rsvp/rsvpinvitation.blade.php
@@ -1,16 +1,15 @@
 @component('mail::message')
-.
 {{$event->name}}
 ----------------
 
-![alt text]({{$image}} "Logo Title Text 1")
+![Logo]({{$image}})
 
 
 
 @component('mail::promotion')
 {{$message}}
 
-@component('mail::button', ['url' => url('/api/rsvp/confirmrsvp/'.$eventUser->id), 'color' => 'green'])
+@component('mail::button', ['url' => url('/api/rsvp/confirmrsvp/'.$eventUser->id), 'color' => 'evius'])
 Confirmar asistencia
 @endcomponent
 
@@ -22,16 +21,16 @@ Confirmar asistencia
 -----------------------
 @endcomponent
 @component('mail::table')
-| Cuando                | Lugar              | 
-| --------------------  |:------------------:| 
-| Fecha:                |                    | 
-| {{$event->date_start}}|  {{$event->venue}} | 
-|  Hora:                | Dirección:         |
-|  {{$event->hour}}     |{{$event->location}}| 
+| Cuando                | Lugar               | 
+| --------------------  |:-------------------:| 
+| **Fecha:**            |                     | 
+| {{$event->date_start}}| {{$event->venue}}   | 
+| **Hora:**             | **Dirección:**      |
+| {{$event->hour}}      | {{$event->location}}| 
 
 @endcomponent
 
-@component('mail::button', ['url' => url('/api/rsvp/confirmrsvp/'.$eventUser->id), 'color' => 'green'])
+@component('mail::button', ['url' => url('/api/rsvp/confirmrsvp/'.$eventUser->id), 'color' => 'evius'])
 Confirmar asistencia
 @endcomponent
 
