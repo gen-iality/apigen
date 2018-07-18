d003fe6e2cf76548ccb607ffe479165d8b9845fb
wittolf <juan.lopez@mocionsoft.com>
2018-07-18 06:56:10 +0100

    dynamic cors allow origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,8 +8,14 @@ class Cors {
     public function handle($request, Closure $next)
     {
         
+        $urlComponent = parse_url($_SERVER['HTTP_REFERER']);
+        $originURL = $urlComponent["scheme"]."://".$urlComponent["host"];
+        if(isset($urlComponent["port"])) {
+            $originURL .= ":".$urlComponent["port"];
+        }
+                
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Origin', $originURL)
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-XSRF-TOKEN");
