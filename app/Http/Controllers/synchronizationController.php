f736a59c0a67e21f13b7fd4e1392513d0d3bdba7
Felipe Martinez <felipe.martinez@mocionsoft.com>
2018-10-30 11:44:28 -0500

    integration firebase with php was developed

--- app/Http/Controllers/synchronizationController.php
+++ app/Http/Controllers/synchronizationController.php
@@ -2,84 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use Morrislaptop\Firestore\Factory;
+use Kreait\Firebase\ServiceAccount;
 use Illuminate\Http\Request;
+use App\EventUser;
 
 class synchronizationController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Event Users
+     * 
+     * Este controlador fue diseñado para exportar todos los event_users que se encuentran en mongo
+     * Realizando una migración completa,
      *
+     * para mas información acerca del funcionamiento de firestore con php sigue el siguiente link
+     * https://github.com/morrislaptop/firestore-php
+     * 
+     * El controlador sigue los siguientes pasos:
+     *      1. Se abre el servicio de firestore
+     *      2. Captura toda la información de la table event_users
+     *      3. Crea la collección, el cual es el mismo nombre "event_users"
+     *      4. Recorre todos los usuarios encontrados anteriormente pero.
+     *          4.1. Si los datos del usuario existen entonces.
+     *          4.2. Guarda un nuevo documento con el id del event_user.
+     *          4.3. Convertimos los datos del usuario en un array para poder guardarlo. 
+     *          4.4. Dentro del documento guardamos los datos del usuario.
+     *      5. Al finalizar retornamos un mensaje sobre la culminación del trabajo
+     * 
+     * Inconvenientes: La cantidad de usuarios, hace que la página no responda arrogando un
+     * error por limite de tiempo.
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function EventUsers()
     {
-        //
-        return "Hello World";
+        try{
+            $serviceAccount = ServiceAccount::fromJsonFile(base_path('firebase_credentials.json'));
+            $firestore = (new Factory)->withServiceAccount($serviceAccount)->createFirestore();
+            $eventUsers = EventUser::all();
+            $collection = $firestore->collection('event_users');
+            foreach($eventUsers as $eventUser){
+                if($eventUser->user){
+                    $user = $collection->document($eventUser->_id);
+                    $dataUser = json_decode($eventUser->user,true);
+                    $user->set($dataUser);
+                }
+            }
+            return  response('the proccess was completed');
+        } catch(exception $e){
+            return $e->messsage();
+        }
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Event User
+     * 
+     * Este controlador fue diseñado para exportar un event_user que se encuentran en mongo
+     * Realizando una migración por medio del id,
      *
+     * para mas información acerca del funcionamiento de firestore con php sigue el siguiente link
+     * https://github.com/morrislaptop/firestore-php
+     * 
+     * El controlador sigue los siguientes pasos:
+     *      1. Se abre el servicio de firestore
+     *      2. Captura toda la información del event_users
+     *      3. Se diríge a la collección, el cual es el mismo nombre "event_users"
+     *      4. Recorre todos los usuarios encontrados anteriormente pero.
+     *          4.1. Si los datos del usuario existen entonces.
+     *          4.2. Guarda un nuevo documento con el id del event_user.
+     *          4.3. Convertimos los datos del usuario en un array para poder guardarlo. 
+     *          4.4. Dentro del documento guardamos los datos del usuario.
+     *      5. Al finalizar retornamos un mensaje sobre la culminación del trabajo
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function EventUser($user)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $serviceAccount = ServiceAccount::fromJsonFile(base_path('firebase_credentials.json'));
+        $firestore = (new Factory)->withServiceAccount($serviceAccount)->createFirestore();
+        $eventUser = EventUser::find($user);
+        
+        if($eventUser->user){
+            $collection = $firestore->collection('event_users');
+            $user = $collection->document($eventUser->_id);
+            $dataUser = json_decode($eventUser->user,true);
+            $user->set($dataUser);
+        }
+        return  response('the proccess was completed');
     }
 }
