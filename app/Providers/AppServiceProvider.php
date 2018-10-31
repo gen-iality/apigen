1ccf64ea07f5a281161b24dd8cfdce375a04c7ec
Felipe Martinez <felipe.martinez@mocionsoft.com>
2018-10-31 14:15:21 -0500

    conditional

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -51,7 +51,11 @@ class AppServiceProvider extends ServiceProvider implements ShouldQueue
                  *      2. El id del DOCUMENTO
                  *      3. La información que desear guardar en el documento COLLECCIÓN.
                  */
-                self::saveFirebase('users', $eventUser->user->_id, $eventUser);
+                if($eventUser->properties){
+                    self::saveFirebase('users', $eventUser->user->_id, $eventUser->properties);
+                }else{
+                    self::saveFirebase('users', $eventUser->user->_id, $eventUser);
+                }
 
             if ($eventUser->state_id == EventUser::STATE_BOOKED) {
 
