e5be05b3459fd743d9ca5054476791d0086878e2
jairo munoz <jairo.munoz@mocionsoft.com>
2018-11-13 11:10:18 -0500

    test

--- resources/views/bookingConfirmed.blade.php
+++ resources/views/bookingConfirmed.blade.php
@@ -44,7 +44,7 @@ Ubicación del evento  <br>
         A través del sitio web de Evius.co puedes acceder
         fácilmente a las entradas de tus eventos. Comienza
         dando clic en el siguiente enlace para confirmar tu
-        cuenta y ver los eventos que esperan por ti
+        cuenta y ver los eventos que esperan por ti 
     </span>
 </div>
 
