59337cfde6eb024ba705eaf6ca7caee5250f1591
Felipe Martinez <felipe.martinez@mocionsoft.com>
2018-11-15 10:52:07 -0500

    email

--- config/database.php
+++ config/database.php
@@ -16,7 +16,6 @@ if ($config->isAvailable()){
     // putenv("QUEUE_DRIVER={$redis['scheme']}");
     putenv("REDIS_HOST={$redis['host']}"); 
     putenv("REDIS_PORT={$redis['port']}");
-var_dump($database);
 }
 // End offset extraction
 
