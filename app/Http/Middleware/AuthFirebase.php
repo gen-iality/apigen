0e256f9d057ccea6bdbbec6a7630503b192a2e07
devmocionsoft <apps@mocionsoft.com>
2018-11-17 21:19:08 +0000

    test

--- app/Http/Middleware/AuthFirebase.php
+++ app/Http/Middleware/AuthFirebase.php
@@ -14,10 +14,12 @@ class AuthFirebase
 {
 
     protected $auth;
+    protected $identificador;
 
     public function __construct(\Kreait\Firebase\Auth $auth)
     {
         $this->auth = $auth;
+        $this->id = time();
     }
 
     /**
@@ -42,7 +44,7 @@ class AuthFirebase
      */
     public function handle(\Illuminate\Http\Request $request, Closure $next)
     {
-        Log::debug('Init authfirebase' . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . __LINE__);
+        Log::debug('Init authfirebase' . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . $this->id);
         try {
             /**
              * Se carga el sdk de firebase para PHP
@@ -97,11 +99,11 @@ class AuthFirebase
             $verifiedIdToken = $verifier->verifyIdToken($firebaseToken);
             $user = self::validator($verifiedIdToken, $refresh_token);
             $request->attributes->add(['user' => $user]);
-            Log::debug("finish auth: " . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . __LINE__);
+            Log::debug("finish auth: " . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . $this->id);
             return $next($request);
 
         } catch (\Firebase\Auth\Token\Exception\ExpiredToken $e) {
-            Log::debug("token expirado renovando" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . __LINE__);
+            Log::debug("token expirado renovando" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . " " . $this->id);
             /**
              * Decodificación del token
              * Para decodificar utilizamos JWT https://firebase.google.com/docs/auth/admin/verify-id-tokens
@@ -170,10 +172,10 @@ class AuthFirebase
             $user = self::validator($verifiedIdToken, $refresh_token = null);
 
             $request->attributes->add(['user' => $user]);
-            Log::debug("finish refreshtoken" . __LINE__);
+            Log::debug("finish refreshtoken" . $this->id);
             return $next($request)->header('new_token', $token_response->access_token);
         } catch (\Exception $e) {
-            Log::debug("bug: " . $e->getMessage() . __LINE__);
+            Log::debug("bug: " . $e->getMessage() . $this->id);
             return response(
                 [
                     'status' => Response::HTTP_UNAUTHORIZED,
@@ -202,11 +204,11 @@ class AuthFirebase
     {
 
         try {
-            Log::debug("buscando un usuario:" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . __LINE__);
+            Log::debug("buscando un usuario:" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . $this->id);
             $user_auth = $this->auth->getUser($verifiedIdToken->getClaim('sub'));
             $user = User::where('uid', '=', $user_auth->uid)->first();
             if (!$user) {
-                Log::debug("Creando un nuevo usuario:" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . __LINE__);
+                Log::debug("Creando un nuevo usuario:" . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . $this->id);
                 $user = User::create(get_object_vars($user_auth));
             }
             if ($refresh_token) {
@@ -215,7 +217,7 @@ class AuthFirebase
             }
 
         } catch (\Exception $e) {
-            Log::debug("bug: " . $e->getMessage() . __LINE__);
+            Log::debug("bug: " . $e->getMessage() . $this->id);
             return response(
                 [
                     'status' => Response::HTTP_UNAUTHORIZED,
