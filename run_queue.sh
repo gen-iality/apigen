ed3eb8dbbc0e52c251a17b3bb017bf8c6dec8db4
devmocionsoft <apps@mocionsoft.com>
2018-11-20 04:42:08 +0000

    adding sh to run queue

--- run_queue.sh
+++ run_queue.sh
@@ -4,12 +4,12 @@
 # -x flag only match processes whose name (or command line if -f is
 # specified) exactly match the pattern. 
 
-if pgrep -f "php ../../../artisan queue:work" > /dev/null
+if pgrep -f "php /app/artisan queue:work" > /dev/null
 then
     echo "Running"
-    php ../../../artisan queue:restart
+    php /app/artisan queue:restart
     pwd
 else
-    php ../../../artisan queue:work  &
+    php /app/artisan queue:work  &
     pwd
 fi
