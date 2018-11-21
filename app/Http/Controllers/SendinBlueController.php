28281b0ad05679cfd86a039b6fd11f7da473d8d0
jairo munoz <jairo.munoz@mocionsoft.com>
2018-11-21 15:57:16 -0500

    add total message status2

--- app/Http/Controllers/SendinBlueController.php
+++ app/Http/Controllers/SendinBlueController.php
@@ -138,13 +138,6 @@ class SendinBlueController extends Controller
                 array_push($array, $user_status);    
                 $message_user->history = $array;
             }
-
-            $message = Message::findOrfail($message_user->message_id);
-            $add_status = $message->$user_status;
-            $message->$user_status = $add_status + 1;
-            
-            $message->save(); 
-            
             $message_user->save(); 
 
         }catch(\Exception $e){
