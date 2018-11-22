d78afccf05b6291ab0e0347c8bc1b4a2457d68c0
devmocionsoft <apps@mocionsoft.com>
2018-11-22 14:01:10 +0000

    space

--- app/Events/Event.php
+++ app/Events/Event.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
+
 class Event
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
