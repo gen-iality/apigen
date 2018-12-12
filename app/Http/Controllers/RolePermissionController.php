2eb27277d92aa34dd8491664f61245c7de94e659
Felipe Martinez <felipe.martinez@mocionsoft.com>
2018-12-12 09:46:23 -0500

    rolepermissions was created

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Http\Request;
+use App\User;
+use App\EventUser;
+
+class RolePermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $data = $request->json()->all();
+        $role_id = $data['role_id'];
+        $user_id = $data['user_id'];
+
+
+        $user = User::findOrFail($user_id);
+        $user->update($request->all());
+        $roles = $request->input('roles') ? $request->input('roles') : [];
+        $user->syncRoles($roles);
+
+
+        return 
+
+
+        $role = Role::find($role_id);
+        $user = User::find($user_id);
+        return $user->role();
+        // $user->assignRole('Administrator');
+        
+        return Role::find();
+        return Role::all();
+
+        // return $request;
+        // Role::create(['name' => 'writer']);
+        // Permission::create(['name' => 'edit articles']);
+        $role = Role::find('5c080634f33bd4188832dfa6');
+        $permission = Permission::create(['name' => 'edit articles']);
+        $permissionB = Permission::create(['name' => 'deleted articles']);
+        $permissionC = Permission::create(['name' => 'new articles']);
+
+        $role->givePermissionTo($permission);
+        $role->givePermissionTo($permissionB);
+        $role->givePermissionTo($permissionC);
+
+        return $role;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $roles = $request->input('roles') ? $request->input('roles') : [];
+        $user->syncRoles($roles);
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
