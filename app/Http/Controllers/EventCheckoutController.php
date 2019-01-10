bd97c168f3b5bf71fdff3e297b51539a21d5958c
Felipe Martinez <felipe.martinez@mocionsoft.com>
2019-01-10 11:26:11 -0500

    it was removed the css in the pdf ticket of the order

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -751,10 +751,13 @@ class EventCheckoutController extends Controller
             'event'     => $order->event,
             'tickets'   => $order->event->tickets,
             'attendees' => $order->attendees,
-            'css'       => file_get_contents(public_path('assets/stylesheet/ticket.css')),
+            //'css'       => "file_get_contents(public_path('assets/stylesheet/ticket.css'))",
+            'css' => '',
             //'image'     => base64_encode(file_get_contents(public_path($order->event->organiser->full_logo_path))),
             'images'    => $images,
         ];
+
+
 
         if ($request->get('download') == '1') {
           //  return PDF::html('Public.ViewEvent.Partials.PDFTicket', $data, 'Tickets');
