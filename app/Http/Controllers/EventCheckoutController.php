524896aabec2a1efe14d1628202e4f91bf1726c8
eviusco <apps@mocionsoft.com>
2019-01-25 14:03:30 +0000

    email was enabled

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -817,7 +817,7 @@ class EventCheckoutController extends Controller
         }
         
         $order->save();
-        $this->dispatch(new SendOrderTickets($order));
+        $this->dispatch(new \App\Jobs\SendOrderTickets($order));
         return $order;
     }
 
