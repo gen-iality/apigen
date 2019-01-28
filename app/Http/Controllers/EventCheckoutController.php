751544ceb93fd21d5c51611d2aed1f64749f94d1
Felipe Martinez <felipe.martinez@mocionsoft.com>
2019-01-28 16:26:32 -0500

    order cahce was developer

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -276,7 +276,7 @@ class EventCheckoutController extends Controller
 
         //Capturamos los datos ingresados por el usuario y la guardamos en el cache como request_data
         $ticket_order['request_data'] = $request->except(['card-number', 'card-cvc']);
-        Cache::put($temporal_id, $ticket_order, 60);
+        Cache::put($temporal_id, $ticket_order, config('attendize.minutes_cache_tickets'));
 
         //START No entiendo que hace acá esto
         $orderRequiresPayment = $ticket_order['order_requires_payment'];
@@ -412,7 +412,7 @@ class EventCheckoutController extends Controller
                 $ticket_order['transaction_data'] += ['session_id' => $session_id];
                 $ticket_order['transaction_data'] += ['url_redirect' => $url_redirect];
                 //Guardamos la informacion del tickete en el cache y vamos a complete order para generar la orden.
-                Cache::put($temporal_id, $ticket_order, 60);
+                Cache::put($temporal_id, $ticket_order, config('attendize.minutes_cache_tickets'));
                 $this->storeOrder($temporal_id);
                 
 
