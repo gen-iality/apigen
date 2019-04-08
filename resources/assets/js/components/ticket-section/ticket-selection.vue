7847e0a5b004554f091920adb2455cdc5c719569
Felipe Martinez <felipe.martinez@mocionsoft.com>
2019-04-07 21:13:02 -0500

    redirect fixed

--- resources/assets/js/components/ticket-section/ticket-selection.vue
+++ resources/assets/js/components/ticket-section/ticket-selection.vue
@@ -66,7 +66,7 @@
                     v-model="selectQuantity"
                     @change="chartConfiguration"
                   >
-                    <option v-for="idx in 9" :key="idx">{{idx}}</option>
+                    <option v-for="idx in 2" :key="idx">{{idx}}</option>
                   </select>
                 </div>
 
