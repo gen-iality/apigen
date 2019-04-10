53bb23ebe541fd14ff05589ebb532b138d6cebb7
Felipe Martinez <felipe.martinez@mocionsoft.com>
2019-04-10 15:14:25 -0500

    one ticket

--- resources/assets/js/components/ticket-section/ticket-selection.vue
+++ resources/assets/js/components/ticket-section/ticket-selection.vue
@@ -66,7 +66,7 @@
                     v-model="selectQuantity"
                     @change="chartConfiguration"
                   >
-                    <option v-for="idx in 2" :key="idx">{{idx}}</option>
+                    <option v-for="idx in 1" :key="idx">{{idx}}</option>
                   </select>
                 </div>
 
