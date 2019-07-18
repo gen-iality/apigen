c1cb937b2b8d132de7d730cd015161166829759b
cesar barriosnuevos <cesar.torres@mocionsoft.com>
2019-07-18 16:55:37 -0500

    fixing badcode discount given

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -249,7 +249,7 @@ class EventCheckoutController extends Controller
 
         $code_discount = $request->get('code_discount');
 
-        if ($code_discount) {
+        if ($code_discount && is_array(codes_discount)) {
             foreach ($event->codes_discount as $code) {
 
                 if ($code['id'] == $code_discount && $code['available'] == true) {
@@ -264,6 +264,7 @@ class EventCheckoutController extends Controller
                             ]
                         );                        
                     }
+
 
 
                     if (isset($code['ticket_assigned'])) {
