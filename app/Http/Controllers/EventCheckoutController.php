623b11be72d501fce8913a0b325401f3683afcd9
cesar barriosnuevos <cesar.torres@mocionsoft.com>
2019-07-19 08:06:05 -0500

    validando solo 1 ticket con cod promocional

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -256,7 +256,7 @@ class EventCheckoutController extends Controller
 
 
                     //ticket amount limit by tickets_amount
-                    if(isset($code['tickets_amount']) && $total_ticket_quantity > 2 ){
+                    if(isset($code['tickets_amount']) && $total_ticket_quantity > $code['tickets_amount']){
                         return response()->json(
                             [
                                 'status' => 'error',
