67fc99c15da80712618585809c9b324f479c02e7
Pablo Triviño <pablo.trivino@mocionsoft.com>
2019-09-24 14:18:23 +0000

    CertificateController.php edited online with Bitbucket

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -240,7 +240,7 @@ class CertificateController extends Controller
      
             //FUNCION DE ENVIAR CORREO
             
-            Mail::send('Public.ViewEvent.Partials.certificate', $data, function ($message) use ($data,$pdf,$cedula,$nombreEvento){
+            Mail::send('Public.ViewEvent.Partials.certificate', "Tus certificados del evento".$nombreEvento , function ($message) use ($data,$pdf,$cedula,$nombreEvento){
                 $message->to($cedula,"Evento PMI")
                 ->subject("Tus certificados para el evento",$nombreEvento)
                 ->attachData($pdf->download(),'Tickets.pdf');
