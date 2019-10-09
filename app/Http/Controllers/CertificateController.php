fde56e116688707473bfcef57f4784504ec92ba1
pablotrivino <pablo.trivino@mocionsoft.com>
2019-10-09 13:44:45 -0500

    send email to all PMI users

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -216,7 +216,7 @@ class CertificateController extends Controller
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="iden">'.$data_single->{"identificacion"}.'</div><br><span style="font-style: italic;font-size:1.6em">participó en calidad de</span><br><span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br><span style="font-style: italic;font-size:1.2em" class="eventName">al VIII Congreso Internacional de Gerencia de Proyectos que se realizó<br>los días 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br>12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        Mail::raw("Tus certificados para el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019 se encuentran adjuntos, recuerda descargarlos y notificar si encuentras errores en tus datos." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
@@ -230,11 +230,11 @@ class CertificateController extends Controller
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="mental">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>La mentalidad ágil como una fortaleza de resultado y generación de valor</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                       /* Mail::raw("Tus certificados para el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019 se encuentran adjuntos, recuerda descargarlos y notificar si encuentras errores en tus datos." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
-                        });  */
+                        });  
                         return $pdf->download('Tickets.pdf');
                         break;   
                     }
@@ -246,11 +246,11 @@ class CertificateController extends Controller
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="desing">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>¿Es posible pensar diferente? Una experiencia real de Design Thinking</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        /*Mail::raw("Tus certificados para el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019 se encuentran adjuntos, recuerda descargarlos y notificar si encuentras errores en tus datos." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
-                        });  */
+                        });  
                         return $pdf->download('Tickets.pdf');
                         break;  
                     }   
@@ -262,19 +262,17 @@ class CertificateController extends Controller
                             $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span><br><div style="position:fixed;display:none" class="coach">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>El Gerente Coach en acción</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                       /* Mail::raw("Tus certificados para el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019 se encuentran adjuntos, recuerda descargarlos y notificar si encuentras errores en tus datos." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
-                            });  */
+                            });  
                         return $pdf->download('Tickets.pdf');
                         break;  
                     }
                 
                 }    
-                $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
-                    $pdf->setPaper( 'letter',  'landscape' );
-                    return $pdf->download('Tickets.pdf');
+               
         }
         return view('Public.ViewEvent.Partials.PDFTicket', $data);
     }
