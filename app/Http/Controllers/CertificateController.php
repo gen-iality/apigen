3ef2e84a0aff9ca68ff6a102f80b008cf080fafc
pablotrivino <pablo.trivino@mocionsoft.com>
2019-10-09 15:08:16 -0500

    send email to all PMI users

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -196,27 +196,30 @@ class CertificateController extends Controller
 
     public function generateCertificate(Request $request)
     {
-        $data = $request->json()->all();
-        //$content = '<p><br></p> <p> <h3>CERTIFICADO DE ASISTENCIA</h3> </p> </br> <p style="margin-top:-3%;" ><span style="font-weight: 400; font-size: 14pt;"> <br></span></p> <p style="color:#5E605E">Certificamos que&nbsp;<span style="font-style: normal; font-weight: bold;" class="name">Pablo </span> , identificada con el No. de cédula<br> <span style="font-style: normal; font-weight: bold;" class="iden">[1033801141user.identificación]</span> participó con éxito en calidad de asistente&nbsp;<span style="font-style: normal; font-weight: bold;"><br class="eventName">[event.name]</span></p><br><p style="color:#5E605E">BOGOTÁ, COLOMBIA</p> <div style="position:absolute;bottom: 420px;left:-1440px"><span style="font-style: normal; font-weight: bold;">DOMINICA MARTÍNEZ</span><p>presidente Congreso Internacional de<br>Gerencia de Proyectos</p></div> <div style=" position:absolute;bottom:490px;right:-1540px;"><span style="font-style:normal;font-weight: bold">CLAUDIA TRUJILLO</span><p>presidente PMI - 2019</p></div>';
+        //$data = $request->json()->all();
+        //
+       
+        $content = '<p><br></p> <p> <h3>CERTIFICADO DE ASISTENCIA</h3> </p> </br> <p style="margin-top:-3%;" ><span style="font-weight: 400; font-size: 14pt;"> <br></span></p> <p style="color:#5E605E">Certificamos que&nbsp;<span style="font-style: normal; font-weight: bold;" class="name">Pablo </span> , identificada con el No. de cédula<br> <span style="font-style: normal; font-weight: bold;" class="iden">[1033801141user.identificación]</span> participó con éxito en calidad de asistente&nbsp;<span style="font-style: normal; font-weight: bold;"><br class="eventName">[event.name]</span></p><br><p style="color:#5E605E">BOGOTÁ, COLOMBIA</p> <div style="position:absolute;bottom: 420px;left:-1440px"><span style="font-style: normal; font-weight: bold;">DOMINICA MARTÍNEZ</span><p>presidente Congreso Internacional de<br>Gerencia de Proyectos</p></div> <div style=" position:absolute;bottom:490px;right:-1540px;"><span style="font-style:normal;font-weight: bold">CLAUDIA TRUJILLO</span><p>presidente PMI - 2019</p></div>';
       
-        //$data = [
-        //    'content'   => $content,
-        //    'image'     => "ASDASD"
-        //];
-   
+        $data = [
+            'content'   => $content,
+            'image'     => "ASDASD"
+        ];
             
             if ($request->get('download') == '1') {
                 
                 $evento = $data["content"];
+               
                 if(strpos($evento, 'class="iden"') ){
                     $contentqry = Attendee::where("event_id","5d9de5e3d74d5c0594053262")->get();
                     //$contentqry = Attendee::where("event_id","5d2de182d74d5c28047d1f85")->get();
                     foreach($contentqry as $datos){
+                        break;
                         $data_single = json_decode(json_encode($datos,true));
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="iden">'.$data_single->{"identificacion"}.'</div><br><span style="font-style: italic;font-size:1.6em">participó en calidad de</span><br><span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br><span style="font-style: italic;font-size:1.2em" class="eventName">al VIII Congreso Internacional de Gerencia de Proyectos que se realizó<br>los días 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br>12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, link bit.ly/memoriaspmi2019 ." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
@@ -226,11 +229,12 @@ class CertificateController extends Controller
                 }else if(strpos($evento, 'class="mental"') ){
                     $contentqry = Attendee::where("event_id","5d9de5e3d74d5c0594053262")->get();
                     foreach($contentqry as $datos){
+                        break;
                         $data_single = json_decode(json_encode($datos,true));
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="mental">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>La mentalidad ágil como una fortaleza de resultado y generación de valor</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, link bit.ly/memoriaspmi2019 ." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
@@ -242,11 +246,12 @@ class CertificateController extends Controller
 
                     $contentqry = Attendee::where("event_id","5d9de5e3d74d5c0594053262")->get();
                     foreach($contentqry as $datos){
+                        break;
                         $data_single = json_decode(json_encode($datos,true));
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span> <br> <div style="position:fixed;display:none" class="desing">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>¿Es posible pensar diferente? Una experiencia real de Design Thinking</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, link http://bit.ly/2p5RmGB ." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
@@ -258,11 +263,12 @@ class CertificateController extends Controller
 
                     $contentqry = Attendee::where("event_id","5d9de5e3d74d5c0594053262")->get();
                     foreach($contentqry as $datos){
+                        break;
                         $data_single = json_decode(json_encode($datos,true));
                         $data['content'] = '<br><br><br><br><br><br><br><br><br><p><br></p> <p> </p> <h3 style="font-size:2.5em;font-style: italic;">El PMI Bogotá Colombia Chapter</h3> <span style="font-style: italic;font-size:1.6em">Certifica que</span> <br> <span style="font-style: italic; font-weight: bold;font-size:2em;color:black;">'.$data_single->{"nombres"}.'</span><br><div style="position:fixed;display:none" class="coach">'.$data_single->{"identificacion"}.'</div><br> <span style="font-style: italic;font-size:1.6em">participó en calidad de</span> <br> <span style="font-style: italic;font-weight: bold;font-size:1.9em;color:black">Asistente</span> <br><br> <span style="font-style: italic;font-size:1.2em" class="eventName">en la jornada de taller <strong>El Gerente Coach en acción</strong><br>y en el VIII Congreso Internacional de Gerencia de Proyectos que se realizaron<br>los días 26, 27 y 28 de septiembre de 2019 en la ciudad de Bogotá, Colombia</span></p> <div style="position:absolute;bottom:20%;right:200px;color:#5E605E;font-style: italic;">Se certifica asistencia de<br> 4 horas de Taller<br> 12 horas de Congreso</div>';
                         $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
                         $pdf->setPaper( 'letter',  'landscape' );
-                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, https://drive.google.com/drive/folders/1ffG6-3DXaVa8br7Lxw2KeP9sJAFdFA_x." , function ($message) use ($data,$pdf,$data_single){
+                        Mail::raw("Mediante este link puedes encontrar las presentaciones compartidas por los conferencistas, link http://bit.ly/2p5RmGB ." , function ($message) use ($data,$pdf,$data_single){
                             $message->to($data_single->{'email'},"Evento PMI")
                             ->subject("Tus certificados para ","el VIII Congreso Internacional de Gerencia de Proyectos Bogotá 2019")
                             ->attachData($pdf->download(),'Tickets.pdf');
@@ -272,7 +278,9 @@ class CertificateController extends Controller
                     }
                 
                 }    
-               
+                $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
+                $pdf->setPaper( 'letter',  'landscape' );
+                return $pdf->download('Tickets.pdf');
         }
         return view('Public.ViewEvent.Partials.PDFTicket', $data);
     }
