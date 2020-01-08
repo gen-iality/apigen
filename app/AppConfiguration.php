ac1b55d94107bad862ca421704f213b2645af469
pablotriviño <pablo.trivino@mocionsoft.com>
2020-01-08 15:35:52 -0500

    api for app configuration

--- app/AppConfiguration.php
+++ app/AppConfiguration.php
@@ -14,10 +14,7 @@ class AppConfiguration extends Moloquent
 {
 
     //protected $with = ['staff'];
-    public function event()
-    {
-        return $this->belongsTo('App\Event');
-    }
+   
 
   
     protected $guarded = [];
