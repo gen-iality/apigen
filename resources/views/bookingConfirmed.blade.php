55e21c641b1c170c8ed9343079009619b33ad779
Pablo Triviño <pablo.trivino@mocionsoft.com>
2020-02-03 15:12:59 +0000

    bookingConfirmed.blade.php edited online with Bitbucket

--- resources/views/bookingConfirmed.blade.php
+++ resources/views/bookingConfirmed.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
 
 {{$event->name}}
+<img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/WhatsApp%20Image%202020-01-31%20at%204.38.14%20PM.jpeg?alt=media&token=6525767f-1c97-4cce-85d1-ecdc8277cea9">
  <!-- ![Evius]({{$logo}}) -->
 Hola {{$eventuser_name}}, has sido invitad@ a {{$event->name}}
 <div style="text-align: justify">
