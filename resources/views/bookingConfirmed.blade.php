17a63c57c85bd9365879bd4f2a1c0d58e5a8bab1
laura prieto <laura.prieto@mocionsoft.com>
2020-02-03 21:26:31 +0000

    bookingConfirmed.blade.php edited online with Bitbucket

--- resources/views/bookingConfirmed.blade.php
+++ resources/views/bookingConfirmed.blade.php
@@ -17,7 +17,7 @@ Hola {{$eventuser_name}}, has sido invitad@ a {{$event->name}}
 <!-- <img src="https://api.evius.co/api/generatorQr/{{$eventuser_id}}"> -->
 
 <div style=" display: block;">
-<span>&nbsp;</span><img style=" display:block; margin:0 50px; text-align: center" src="{{$qr}}" />
+<img style=" display:block; margin:0 50px; text-align: center" src="{{$qr}}" />
 </div>
 
 @component('mail::table')
