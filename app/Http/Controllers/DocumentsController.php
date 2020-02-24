6eb32ca0fbfa2420811f952e1aee78c93b9b5984
pablotriviño <pablo.trivino@mocionsoft.com>
2020-02-24 17:57:13 -0500

    api for documents its better, faster and stronger

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -21,8 +21,14 @@ class DocumentsController extends Controller
      */
     public function index(Request $request, $event_id)
     {
+        $data = $request->json()->all();
+        if(!empty($data["father_id"])){
+            return JsonResource::collection(
+                Documents::where("event_id", $event_id)->where("father_id", $data["father_id"])->paginate(config('app.page_size'))
+            );
+        }
         return JsonResource::collection(
-            Documents::where("event_id", $event_id)->paginate(config('app.page_size'))
+            Documents::where("event_id", $event_id)->where("state", "father")->paginate(config('app.page_size'))
         );
     }
 
@@ -34,11 +40,20 @@ class DocumentsController extends Controller
      */
     public function store(Request $request, $event_id)
     {
-        $data = $request->json()->all();
-        $data["event_id"] = $event_id;
-        $result = new Documents($data);
-        $result->save();
-        return $result;
+        if(!empty($data["father_id"])){
+            $data = $request->json()->all();
+            $data["event_id"] = $event_id;
+            $data["state"] = "child";     
+            $result = new Documents($data);
+                $result->save();
+                return $result;
+            }
+
+            $data["event_id"] = $event_id;     
+            $data["state"] = "father"; 
+            $result = new Documents($data);
+                $result->save();
+                return $result;
     }
 
 
