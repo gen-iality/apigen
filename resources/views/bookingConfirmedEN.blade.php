47290227c4f73aa24d0d17470e55fe8aceeec1d3
pablotriviño <pablo.trivino@mocionsoft.com>
2020-02-28 12:17:40 -0500

    modifying form to bancolombia

--- resources/views/bookingConfirmedEN.blade.php
+++ resources/views/bookingConfirmedEN.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-{{$event->description}}
+{{$event->name}}
 <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/output-onlinepngtools%20(6).png?alt=media&token=106a0f27-a2f8-48f8-a19b-9a936aac420a">
  <!-- ![Evius]({{$logo}}) -->
 Hi {{$eventuser_name}}, you have been invited to {{$event->name}} event
-<div style="text-align: justify">
+<div style="text-align: center">
     <span>
     {{$event->description}}
     <!--Here you can find the event information
