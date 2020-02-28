d83c6d5fffb40c204f32da55616286087517f357
pablotriviño <pablo.trivino@mocionsoft.com>
2020-02-27 19:14:30 -0500

    change language to mail

--- app/Mail/BookingConfirmed.php
+++ app/Mail/BookingConfirmed.php
@@ -68,7 +68,7 @@ class BookingConfirmed extends Mailable implements ShouldQueue
         $this->attach = $attachPath;
         Log::debug("Construyendo el correo de ticket");
         $gfService = new GoogleFiles();
-        echo $this->eventuser_lan;die;
+        echo $this->eventuser_lan;
         $from = $this->event->organizer->name;
         $logo_evius = 'images/logo.png';
         $file = $this->eventuser_id . '_qr.png';
