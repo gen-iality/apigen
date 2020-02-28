5cddc89b1c603427100652710fc0f1f168b46975
pablotriviño <pablo.trivino@mocionsoft.com>
2020-02-28 11:03:05 -0500

    modifying form to bancolombia

--- resources/views/bookingConfirmedEN.blade.php
+++ resources/views/bookingConfirmedEN.blade.php
@@ -27,6 +27,7 @@ Hi {{$eventuser_name}}, you have been invited to {{$event->name}} event
 |VIP | Invitation |
 |<hr>| <hr>|
 | **Date Start:**             | **Time Start:**                                                                 |
+|:----------------------:|:------------------------------------------------------------------------------:|
 | {{ date('l, F j Y ', strtotime($event->datetime_from)) }} | {{date('H:i:s', strtotime($event->datetime_from)) }} |
 |<br>                   |<br>
 
