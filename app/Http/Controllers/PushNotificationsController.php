57ceff218f6b8c712365381bafc41b98692dd1ff
pablotriviño <pablo.trivino@mocionsoft.com>
2020-02-29 16:38:21 -0500

    notifications by bell

--- app/Http/Controllers/PushNotificationsController.php
+++ app/Http/Controllers/PushNotificationsController.php
@@ -31,7 +31,7 @@ class PushNotificationsController extends Controller
     {
 
         $notification = PushNotification::where("event_id", $event_id)->where("User_ids",$id)->get();
-        echo(var_dump($notification["User_ids"]));die;
+        echo(var_dump($notification["id"]));die;
         $userdelete = array_splice($notification['User_ids'],$id);
         $save = PushNotification::find($notification['_id']);
         $save->fill($userdelete);
