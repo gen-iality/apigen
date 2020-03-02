91970ebed8ac772c2c3556ef80d7ebd3ab35b8ea
pablotriviño <pablo.trivino@mocionsoft.com>
2020-03-02 15:42:04 -0500

    new desing for bancolombia

--- resources/views/bookingConfirmedEN.blade.php
+++ resources/views/bookingConfirmedEN.blade.php
@@ -3,8 +3,9 @@
 {{$event->name}}
 
 @slot('footer')
-@component('mail::footer')
 <img style="width:100%;height:auto;" src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/bancolombia%2FCORTADO_LandingBID_LandingENG3.jpg?alt=media&token=c0f06046-b866-4a1a-b44c-42e0a3e1102d">
+
+@component('mail::footer')
 
         © 2001-2020. All Rights Reserved - Evius.co
         is a registered trademark of MOCION
