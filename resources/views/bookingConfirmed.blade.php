6f610b094fe7ebf0a8f412d1297138e7761cea1f
pablotriviño <pablo.trivino@mocionsoft.com>
2020-03-02 15:29:14 -0500

    new desing for bancolombia

--- resources/views/bookingConfirmed.blade.php
+++ resources/views/bookingConfirmed.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 
 {{$event->name}}
-<img style="width:100%;height:auto;" src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/bancolombia%2FLandingBID_LandingESP3.jpg?alt=media&token=7179d085-133d-4852-bed2-24e8e76a5c45">
+<img style="width:100%;height:auto;" src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/bancolombia%2FCORTADO_LandingBID_LandingESP3.jpg?alt=media&token=ba7c6679-4d6e-4be8-91e2-e71b94562aad">
 
 @slot('footer')
 @component('mail::footer')
--- resources/views/bookingConfirmedEN.blade.php
+++ resources/views/bookingConfirmedEN.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 
 {{$event->name}}
-<img style="width:100%;height:auto;" src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/output-onlinepngtools%20(6).png?alt=media&token=106a0f27-a2f8-48f8-a19b-9a936aac420a">
+<img style="width:100%;height:auto;" src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/bancolombia%2FCORTADO_LandingBID_LandingENG3.jpg?alt=media&token=c0f06046-b866-4a1a-b44c-42e0a3e1102d">
 
 @slot('footer')
 @component('mail::footer')
