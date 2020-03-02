866a387076f8e8490b10b4ba8c7f6d592dbf56b3
pablotriviño <pablo.trivino@mocionsoft.com>
2020-03-02 15:08:45 -0500

    new desing for bancolombia

--- resources/views/bookingConfirmed.blade.php
+++ resources/views/bookingConfirmed.blade.php
@@ -1,5 +1,6 @@
 @component('mail::message')
 
+{{$event->name}}
 <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/bancolombia%2FLandingBID_LandingESP3.jpg?alt=media&token=7179d085-133d-4852-bed2-24e8e76a5c45">
 
 @slot('footer')
--- resources/views/bookingConfirmedEN.blade.php
+++ resources/views/bookingConfirmedEN.blade.php
@@ -1,5 +1,6 @@
 @component('mail::message')
 
+{{$event->name}}
 <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/output-onlinepngtools%20(6).png?alt=media&token=106a0f27-a2f8-48f8-a19b-9a936aac420a">
 
 @slot('footer')
