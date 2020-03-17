00185864630b0c84fc2068cc2e162e1e243e88f0
pablotriviño <pablo.trivino@mocionsoft.com>
2020-03-17 12:08:43 -0500

    role to attendees

--- app/Attendee.php
+++ app/Attendee.php
@@ -18,7 +18,7 @@ class Attendee extends Models\Attendee
     protected $observables = ['saved', 'created','updated'];
     protected static $unguarded = true;
     protected $fillable = ['account_id', 'event_id', 'state_id', "checked_in", "checked_in_date", "properties", "activities"];
-    protected $with = ['role_attendees', 'user', 'state' ];
+    protected $with = ['rol_id', 'user', 'state' ];
 
     //Default values
     protected $attributes = [
