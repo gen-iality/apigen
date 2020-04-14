271531e210f12264fc9bee2ac8b75570e4a1cb86
pablotriviño <pablo.trivino@mocionsoft.com>
2020-04-14 09:54:02 -0500

    conditional for event address

--- app/Http/Controllers/RSVPController.php
+++ app/Http/Controllers/RSVPController.php
@@ -84,8 +84,9 @@ class RSVPController extends Controller implements ShouldQueue
         //addUsers - recipients of message
         //https://stackoverflow.com/questions/33005815/laravel-5-retrieve-json-array-from-request
         //$eventUsers = UserEventService::addUsersToAnEvent($event, $eventUsersIds);
-        $eventUsers = UserEventService::addEventUsersToEvent($event, $eventUsersIds);
-        
+        //$eventUsers = UserEventService::addEventUsersToEvent($event, $eventUsersIds);
+
+        $eventUsers = Attendee::find($eventUsersIds);
         $message->number_of_recipients = count($eventUsers);
         $message->save();
 
