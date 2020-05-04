2fe94fe0f1eb285e008a2ae53fcd0cef17f4f8d3
JKLopez <juan.lopez@mocionsoft.com>
2020-05-04 06:47:25 -0500

    probando quitando el botón con el link

--- resources/views/rsvp/rsvpinvitation.blade.php
+++ resources/views/rsvp/rsvpinvitation.blade.php
@@ -2,4 +2,20 @@
 
 Hola {{$eventUser_name}}, estas inscrito en: {{$event->name}}
 
+
+<div style="text-align: center;font-size: 130%;">
+    <p style="font-size: 130%">Para ingresar al evento y ver mas informacion visitanos en:
+        @component('mail::button', ['url' => $link , 'color' => 'evius'])
+        Ingresar a al Evento
+        @endcomponent
+        Tus datos de acceso son:<br>
+
+        Usuario: {{$email}}
+        <br>
+        Código: {{$password}}
+        <br>
+        Te esperamos.
+    </p>
+</div>
+
 @endcomponent
