8d718e1fb4f8f12166e45c49a60957c2d59ba59f
pablotriviño <pablo.trivino@mocionsoft.com>
2020-05-14 09:28:10 -0500

    re desing for rsvp email

--- resources/views/rsvp/rsvpinvitation.blade.php
+++ resources/views/rsvp/rsvpinvitation.blade.php
@@ -4,26 +4,42 @@ Hola {{$eventUser_name}}, estas inscrito en: {{$event->name}}
 
 
 <div style="text-align: center;font-size: 130%;">
-    <p style="font-size: 130%">Para ingresar al evento y ver mas informacion visitanos en:
+    <p style="font-size: 130%">Para ingresar al evento, asistir a la conferencia, ver mas informacion visitanos en:
         @component('mail::button', ['url' => $link , 'color' => 'evius'])
-        Ingresar a al Evento AQUÍ
+        Ingresar al Evento AQUÍ
         @endcomponent
-        Tus datos de acceso son:<br>
-
-        Usuario: {{$email}}
-        <br>
-        Código: {{$password}}
         <br>
         Te esperamos.
+    </p><br><br>
+    <p style="font-size: 15px;color: gray;font-style: italic;">
+    Se recomienda usar los navegadores Google Chrome, Safari o Mozilla Firefox para ingresar a evius, no se recomienda el uso de internet explorer. 
     </p>
+    
     <p>
     @component('mail::promotion')
 
     ![Logo]({{$image}})
+    Si tuviste problemas con el botón de ingreso abre el siguiente enlace <br>
+    Recuerda usar Google Chrome, Safari o Mozilla Firefox.
+
+    <a href="{{$link}}">{{$link}}</a>
         
     @endcomponent
-        <a href="{{$link}}">{{$event->name}}</a>
-    </p>
+
+</div>
+<div style="text-align: center;font-size: 100%;background-color:#ECECEC;padding:20px">
+        <p>Si experimentas problemas al ingresar con los métodos anteriores dale click al botón a continuación "Ingresar a EVIUS" e ingresa el siguiente usuario y contraseña 
+        <br>Tus datos de acceso son:<br>
+        Usuario: {{$email}}
+        <br>
+        Contraseña: {{$password}}
+        <br>
+        @component('mail::button', ['url' => 'https://eviusauth.netlify.app/' , 'color' => 'evius'])
+        Ingresar a EVIUS
+        @endcomponent
+        
+        </p>
+    
 
 </div>
 
