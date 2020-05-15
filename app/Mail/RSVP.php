01a214795f669fdd06c905ef9fffef6243e4eaa2
pablotriviño <pablo.trivino@mocionsoft.com>
2020-05-15 17:09:44 -0500

    revert changes password encryption

--- app/Mail/RSVP.php
+++ app/Mail/RSVP.php
@@ -50,7 +50,7 @@ class RSVP extends Mailable implements ShouldQueue
             $email = $eventUser->properties["email"];
         }
         
-        $password = isset($eventUser["properties"]["password"]) ? $eventUser["properties"]["password"] : self::createPass();    
+        $password = isset($eventUser["properties"]["password"]) ? $eventUser["properties"]["password"] : "evius2020";    
         $eventUser_name = isset($eventUser["properties"]["names"]) ? $eventUser["properties"]["names"] : $eventUser["properties"]["displayName"];
         
         // lets encrypt ! 
