8c234edb92302ef43c6827297483befdc1b5e9ce
pablotriviño <pablo.trivino@mocionsoft.com>
2020-05-19 12:00:24 -0500

    new field for speaker

--- app/Host.php
+++ app/Host.php
@@ -17,5 +17,5 @@ class Host extends Moloquent
     {
         return $this->belongsTo('App\Event');
     }
-    protected $fillable = ['name', 'profession' , 'description' ,'image','event_id', 'en_description', 'profession'];
+    protected $fillable = ['name', 'profession' , 'description' ,'image','event_id', 'en_description', 'profession', 'description_activity'];
 }
