6b3daf555407d1427a9ae1ce635320442312129e
JKLopez <juan.lopez@mocionsoft.com>
2020-05-23 15:39:29 -0500

    probando descripción configurable al correo de bienvenida

--- resources/views/rsvp/rsvpinvitation.blade.php
+++ resources/views/rsvp/rsvpinvitation.blade.php
@@ -1,28 +1,33 @@
 @component('mail::message')
 
-Bienvenid@
+<p>Hola {{$eventUser_name}}, estás inscrito en: {{$event->name}}</p>
 
 @component('mail::promotion')
-    
-    ![Logo]({{$event->styles["banner_image"]}})
-    
+![Logo]({{$event->styles["banner_image"]}})
 @endcomponent
 
-<h1>Hola {{$eventUser_name}}, estás inscrito en: {{$event->name}}</h1>
+
+@if ($event->registration_message)
+
+<div>
+    {{$event->registration_message}}
+</div>
+
+@else
 
 @component('mail::table')
-|                       |                                                                                        | 
-| --------------------  |:--------------------------------------------------------------------------------------:| 
-| **Fecha:**            | **Hora:**                                                                              | 
+| | |
+| -------------------- |:--------------------------------------------------------------------------------------:|
+| **Fecha:** | **Hora:** |
 | {{ date('l, F j Y ', strtotime($event->datetime_from)) }} | {{date('H:i:s', strtotime($event->datetime_from)) }} |
-|<br>                   |<br>  
+|<br> |<br>
 @if($event->datetime_to)
-| **Hasta:**            | **Hora:**                                                                              | 
-| {{ date('l, F j Y ', strtotime($event->datetime_from)) }} | {{date('H:i:s', strtotime($event->datetime_to)) }}   | 
+| **Hasta:** | **Hora:** |
+| {{ date('l, F j Y ', strtotime($event->datetime_from)) }} | {{date('H:i:s', strtotime($event->datetime_to)) }} |
 @endif
-
 @endcomponent
 
+@endif
 
 <div style="text-align: center;font-size: 130%;">
 
@@ -30,30 +35,31 @@ Bienvenid@
         @component('mail::button', ['url' => $link , 'color' => 'evius'])
         Ingresar al Evento AQUÍ
         @endcomponent
-        
+
         <br>
         Te esperamos.
     </p><br><br>
-    
-    
+
+
     {!!$event->description!!}
-    
+
 
 
     <p style="font-size: 15px;color: gray;font-style: italic;">
-    Se recomienda usar los navegadores Google Chrome, Safari o Mozilla Firefox para ingresar a evius, no se recomienda el uso de internet explorer. 
+        Se recomienda usar los navegadores Google Chrome, Safari o Mozilla Firefox para ingresar a evius, no se
+        recomienda el uso de internet explorer.
     </p>
     <hr style="border-right : 0;border-left: 0;">
     <p>
-    Si tuviste problemas con el botón de ingreso abre el siguiente enlace
-    <a href="{{$link}}">click acá</a>
-    Recuerda usar Google Chrome, Safari o Mozilla Firefox.
-        
-    @component('mail::promotion')
-    
-    ![Logo]({{$image}})
-    
-    @endcomponent
+        Si tuviste problemas con el botón de ingreso abre el siguiente enlace
+        <a href="{{$link}}">click acá</a>
+        Recuerda usar Google Chrome, Safari o Mozilla Firefox.
+
+        @component('mail::promotion')
+
+        ![Logo]({{$image}})
+
+        @endcomponent
 
 
 </div>
