fe45773b68bd134f39fbc1f05a87484b85c783b5
JKLopez <juan.lopez@mocionsoft.com>
2020-06-26 16:43:43 -0500

    tamano imagen cambiada prueba 16:9

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -183,9 +183,9 @@ class CertificateController extends Controller
             //return view('Public.ViewEvent.Partials.PDFTicket', $data);
             //return view('Public.ViewEvent.Partials.certificate', $data);
             $pdf = PDF::loadview('Public.ViewEvent.Partials.certificate', $data);
-            $customPaper = array(0, 0, 1920, 1080);
-            //$pdf->setPaper('letter', 'landscape');
-            $pdf->setPaper($customPaper);
+            //$customPaper = array(0, 0, 460, 445);
+            $pdf->setPaper('letter', 'landscape');
+            //$pdf->setPaper($customPaper);
             return $pdf->download('Tickets.pdf');
         }
         return view('Public.ViewEvent.Partials.PDFTicket', $data);
