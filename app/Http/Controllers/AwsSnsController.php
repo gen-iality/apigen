82a98fdd7ca61c2a8c5828a54ddf5e3b0de51351
emjvp <emjvp@gmail.com>
2020-07-14 17:39:33 -0500

    add Log::info('$messageUserModel->response ')

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -17,16 +17,22 @@ class AwsSnsController extends Controller
     public function updateSnsMessages(Request $request)
     {
         
-        $response = $request->json()->all(); 
-        $messageUserModel = new MessageUser($response);
+        $response = $request->json()->all();
+        
+        $messageUserModel = new MessageUser();
+        Log::info('$messageUserModel->response '.$messageUserModel->response);
         // Log::info('request json->all: '.json_encode($response));
 
+
+
+        
 
         // Log::info('request gettype(json->all): '.gettype(json_encode($response)));
         
         // Log::info('request gettype($response): '.gettype($response));
+        
 
-        Log::info('$messageUserModel '.$messageUserModel->status);
+        // Log::info('$messageUserModel '.$messageUserModel->status);
         //$messageUserModel->save();
         
         
