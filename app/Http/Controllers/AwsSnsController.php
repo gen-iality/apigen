53215d242591caaac686f76524856cd6bbff1bfa
emjvp <emjvp@gmail.com>
2020-07-15 15:17:26 -0500

    add         Log::info('json_encode');

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -19,7 +19,7 @@ class AwsSnsController extends Controller
         $response = $request->json()->all();
 
         
-        Log::info('$response->mail->destination '.$response->mail->destination);
+        Log::info('json_encode($response->mail->destination) '.json_encode($response->mail->destination));
 
         $messageUserModel = new MessageUser(
              [
