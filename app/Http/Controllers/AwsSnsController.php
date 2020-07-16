8577036045cc4e1907557dabdd9c49268a59f716
emjvp <emjvp@gmail.com>
2020-07-16 08:39:44 -0500

    Log::info('Message User '.$messageUserModel);

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -19,14 +19,14 @@ class AwsSnsController extends Controller
         $response = $request->json()->all();
 
         // Log::info('$response '.json_encode($response));
-        Log::info('linea 1 $response[mail][messageId] '.$response['mail']['messageId']);
-        Log::info([
-            // 'response' => json_encode($response),
-            'email_destinations' => json_encode($response['mail']['destination']),
-            'status_message' => $response['eventType'],
-            'message_id' => $response['mail']['messageId'],
-            'timestamp_event' => $response['mail']['timestamp']
-        ]);
+        
+        // Log::info([
+        //     'response' => json_encode($response),
+        //     'email_destinations' => json_encode($response['mail']['destination']),
+        //     'status_message' => $response['eventType'],
+        //     'message_id' => $response['mail']['messageId'],
+        //     'timestamp_event' => $response['mail']['timestamp']
+        // ]);
 
         $messageUserModel = new MessageUser(
              [
