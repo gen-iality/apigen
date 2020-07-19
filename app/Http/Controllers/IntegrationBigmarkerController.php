c516f9f338c738632b79e6452c70747a00da3570
JKLopez <juan.lopez@mocionsoft.com>
2020-07-19 07:27:23 -0500

    devolviendo error de la petición de bigmarket

--- app/Http/Controllers/IntegrationBigmarkerController.php
+++ app/Http/Controllers/IntegrationBigmarkerController.php
@@ -13,18 +13,26 @@ class IntegrationBigmarkerController extends Controller
 {
     public function conferencesenter(Request $request)
     {
+        try {
 
-        $data = $request->json()->all();
+            $data = $request->json()->all();
 
-        $url = "https://www.bigmarker.com/api/v1/conferences/enter";
-        //$data["details"] = ["referenceCode" => $order_reference];
-        $client = new Client();
-        $response = $client->request('POST', $url, [
-            'body' => json_encode($data),
-            'headers' => ['API-KEY' => '7b8927965a0dc2b63f85'],
-        ]);
-        $response = $response->getBody()->getContents();
-        return $response;
+            $url = "https://www.bigmarker.com/api/v1/conferences/enter";
+            //$data["details"] = ["referenceCode" => $order_reference];
+            $client = new Client();
+            $response = $client->request('POST', $url, [
+                'body' => json_encode($data),
+                'headers' => ['API-KEY' => '7b8927965a0dc2b63f85'],
+            ]);
+            $response = $response->getBody()->getContents();
+            return $response;
+
+        } catch (\Exception $e) {
+
+            $resp = $e->getResponse()->getBody()->getContents();
+            return response($resp, 400);
+
+        }
 
         //   let data = {
         //     id: "23a1ae7fc1af",
