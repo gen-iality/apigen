1a3342daf324f70f62c88657d11595495113ee0f
emjvp <emjvp@gmail.com>
2020-07-27 16:57:21 -0500

    deleting //comments

--- app/Listeners/LogSentMessage.php
+++ app/Listeners/LogSentMessage.php
@@ -55,26 +55,7 @@ class LogSentMessage
 
 
         $eviusMessage->save();
-
-        // Log::info($event->message->getHeaders());
-
-        // $emailId = $event->message->getId();        
-        // $messagesUser = MessageUser::where('notification_id', $sesMessageId)->get();
-
         
-
-        // Log::info($messagesUser);
-
-        // foreach ($messagesUser as $value)
-        // {
-        //    if (!(isset($value->message_id) && isset($value->message)))
-        //    {
-        //         $value->message_id = $emailId;
-        //         $value->message = $event->message->getBody();
-        //         // $value->save();
-        //    }
-        // }      
-    
     }
 
 }
