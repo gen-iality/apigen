1a2f421a64de0909e4d191bd25816c6c7cc7f7c0
JKLopez <juan.lopez@mocionsoft.com>
2020-09-16 16:41:58 -0500

    subject del registro a actividad quedan unas letras pegadas

--- resources/views/rsvp/activity_registration.blade.php
+++ resources/views/rsvp/activity_registration.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message') 
 <div class="centered"> 
-* Tu registro ha sido exitoso a {{$activity->name}}
+* Tu registro ha sido exitoso a:  {{$activity->name}}
 </div>
 <div class="centered">
 <img src="{{$activity->image}}"></img>
