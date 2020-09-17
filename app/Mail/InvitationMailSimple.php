4fe9532f6e469510eaeb33f73d9c96543913f080
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2020-09-17 11:07:44 -0500

    Agregar icar calendar a los correos de cada actividad

--- app/Mail/InvitationMailSimple.php
+++ app/Mail/InvitationMailSimple.php
@@ -114,6 +114,7 @@ class InvitationMailSimple extends Mailable implements ShouldQueue
                     ->organizer('soporte@evius.co', $event->organizer->name)
                     ->alertMinutesBefore(60, $event->name . " empezará dentro de poco.")
             )->get();
+        var_dump($date_time_from);die;
     }
 
     private function encryptdata($string)
