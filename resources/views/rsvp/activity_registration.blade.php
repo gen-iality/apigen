bcb24a8b60c9939e98ac137953ed1a26c615694d
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2020-09-18 14:04:56 -0500

    Actividades: agregando mensaje personalizado al correo de registro en actividades

--- resources/views/rsvp/activity_registration.blade.php
+++ resources/views/rsvp/activity_registration.blade.php
@@ -1,6 +1,14 @@
 @component('mail::message') 
 <div class="centered"> 
+
+
+<!-- Mensaje personalizado del CMS -->
+@if ($activity->registration_message)
+<p>{{$activity->registration_message}}</p>
+@endif
+
 <p>Tu registro ha sido exitoso a:  {{$activity->name}}</p>
+
 </div>
 
 <div class="centered">
