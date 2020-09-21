0f09ca0c14cd117b502ee1742cb420806af2fd75
JKLopez <juan.lopez@mocionsoft.com>
2020-09-21 08:03:17 -0500

    desarrollo parcial link hacia evius en el icalendar

--- app/Mail/ActivityRegistration.php
+++ app/Mail/ActivityRegistration.php
@@ -1,25 +1,21 @@
 <?php
 
 namespace App\Mail;
-use App\Organization;
-use App\evaLib\Services\GoogleFiles;
+
 use App\Event;
+use App\Organization;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
-use Barryvdh\DomPDF\Facade as PDF;
-use QRCode;
-use Storage;
 use Spatie\IcalendarGenerator\Components\Calendar as iCalCalendar;
 use Spatie\IcalendarGenerator\Components\Event as iCalEvent;
+use Spatie\IcalendarGenerator\PropertyTypes\TextPropertyType as TextPropertyType;
+
 
 class ActivityRegistration extends Mailable implements ShouldQueue
 {
-    use Queueable, SerializesModels;//, Dispatchable, InteractsWithQueue;
+    use Queueable, SerializesModels; //, Dispatchable, InteractsWithQueue;
 
     public $subject;
     public $activityAssistant;
@@ -31,21 +27,29 @@ class ActivityRegistration extends Mailable implements ShouldQueue
      *
      * @return void
      */
-    public function __construct($subject,$activityAssistant,$activity)
+    public function __construct($subject, $activityAssistant, $activity)
     {
         $this->activityAssistant = $activityAssistant;
         $this->activity = $activity;
         $this->subject = $subject;
 
+        $event_link = 'https://evius.co/landing/'.$activity->event->_id;
+
         $datetime_start = \Carbon\Carbon::parse($activity->datetime_start);
-        $datetime_end = \Carbon\Carbon::parse($activity->datetime_end);
+        $datetime_end = ($activity->datetime_end)?\Carbon\Carbon::parse($activity->datetime_end):$datetime_start->addHour();
 
         //Crear un ICAL que es un formato para agregar a calendarios y eso se adjunta al correo
         $this->ical = iCalCalendar::create($activity->name)
+            ->appendProperty(
+                TextPropertyType::create('URL', $event_link) 
+            )
+            ->appendProperty(
+                TextPropertyType::create('X-ALT-DESC', $activity->description.' <a href="'.$event_link.'">visita el evento</a>') 
+            )
             ->event(iCalEvent::create($activity->name)
                     ->startsAt($datetime_start)
                     ->endsAt($datetime_end)
-                    ->description($activity->description)
+                    ->description($activity->description.' <a href="'.$event_link.'">visita el evento</a>')
                     ->uniqueIdentifier($activity->_id)
                     ->createdAt(new \DateTime())
                     ->address(($activity->address) ? $activity->address : "Virtual en web evius.co")
@@ -53,7 +57,9 @@ class ActivityRegistration extends Mailable implements ShouldQueue
                 //->coordinates(51.2343, 4.4287)
                     ->organizer('soporte@evius.co', $activity->organizer)
                     ->alertMinutesBefore(60, $activity->name . " empezará dentro de poco.")
-            )->get();
+            )
+            
+            ->get();
     }
 
     /**
@@ -68,13 +74,12 @@ class ActivityRegistration extends Mailable implements ShouldQueue
 
         $subject = $this->subject;
         return $this
-        ->from("alerts@evius.co", $from)
-        ->subject($subject)
-        ->attachData($this->ical, 'ical.ics', [
-            'mime' => 'text/calendar',
-        ])
-        ->markdown('rsvp.activity_registration');
-        
+            ->from("alerts@evius.co", $from)
+            ->subject($subject)
+            ->attachData($this->ical, 'ical.ics', [
+                'mime' => 'text/calendar;charset=UTF-8;method=REQUEST',
+            ])
+            ->markdown('rsvp.activity_registration');
     }
-    
+
 }
