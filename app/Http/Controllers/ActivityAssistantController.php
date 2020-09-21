5744a3c47fe20f3ab5c9db37b9dd4f4fef62bf8d
JKLopez <juan.lopez@mocionsoft.com>
2020-09-21 09:46:25 -0500

    una prueba para generar link de ingreso

--- app/Http/Controllers/ActivityAssistantController.php
+++ app/Http/Controllers/ActivityAssistantController.php
@@ -108,7 +108,7 @@ class ActivityAssistantController extends Controller
         $activity = Activities::find($data["activity_id"]);
         
 
-        $subject = "Tu registro ha sido exitoso a:  ".$activity->name;
+        $subject = "Confirmación de regsitro ".$activity->name;
         Mail::to($user->email)
         ->queue(
             //string $message, Event $event, $eventUser, string $image = null, $footer = null, string $subject = null)
