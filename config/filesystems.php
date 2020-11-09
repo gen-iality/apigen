58a095e8a17760a9136214c792986b2523088546
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2020-11-09 14:56:58 -0500

    Actualizando ruta en la que se suben archivos

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'eviusauth'),
             'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path("/service-account.json")), // optional: /path/to/service-account.json
-            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'herba-images'),
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'eviusauth.appspot.com'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
         ],
