6bfb758affe44bdf5dbd3e29c9fd157b79481ccf
emjvp_mocion <emilio.vargas@mocionsoft.com>
2020-12-11 10:46:40 -0500

    agregando         Log::info('notificationType '.json_encode($response)['notificationType']); al AwsSnsController.php

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -36,7 +36,7 @@ class AwsSnsController extends Controller
         $response = $request->json()->all();
         Log::info('response '.json_encode($response));
         // Log::info('eventType '.json_encode($response)['eventType']);
-        // Log::info('notificationType '.json_encode($response)['notificationType']);
+        Log::info('notificationType '.json_encode($response)['notificationType']);
         
         $eviusmessage = EviusMessage::where('server_message_id', '=', $response['mail']['messageId'])->first();
 
