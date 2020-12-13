6ec68f13a7fb56c3b4a4b4f6ba39b3cd5f9df0d0
emjvp_mocion <emilio.vargas@mocionsoft.com>
2020-12-13 18:34:56 -0500

    Log::info('print_r($responseMail) '.print_r($responseMail));

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -28,10 +28,10 @@ class AwsSnsController extends Controller
         // Log::info('update');
         $count = 0;
         $response = $request->json()->all();
-        Log::info('response '.json_encode($response));
+        // Log::info('response '.json_encode($response));
         Log::info('print_r($response, true) '.print_r($response, true));
         $responseMail = $response['mail'];
-        Log::info('gettype($responseMail) ',gettype($responseMail));
+        Log::info('print_r($responseMail) '.print_r($responseMail));
 
         // Log::info('eventType '.json_encode($response)['eventType']);
         // Log::info('notificationType '.json_decode($response, true)['notificationType']);
