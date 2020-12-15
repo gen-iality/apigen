e1c213c8f409e9d5874b472f56cc9c7d2bfc816f
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2020-12-14 21:11:18 -0500

    contactbyemail end poitn para realizar envio de formulario de contacto de ucronio

--- resources/views/genericMail.blade.php
+++ resources/views/genericMail.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')  
-    
-    <p><strong>Nombre del usuario: </strong>{{ $userName }}</p>
-    <p><strong>Correo del usuario: </strong>{{ $emailUser }}</p>
+    <div style="text-align: center">
+    <p><strong>Nombre del usuario: </strong>{{$userName}}</p>
+    <p><strong>Correo del usuario: </strong>{{$emailUser}}</p>
     <p>
         {{$message}}
     </p>
-    
+    </div>
 
 @endcomponent
