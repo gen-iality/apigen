311fe2282865ccf5f8c19d0c638f5151d0b8dd23
emjvp_mocion <emilio.vargas@mocionsoft.com>
2020-12-28 00:42:20 -0500

    comentando $dataEviusMessage = [         //     'server_message_id' => $responseMail['messageId']         // ];         // $eviusMessageModel = new EviusMessage($dataEviusMessage);         // $eviusMessageModel->save();

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -32,11 +32,11 @@ class AwsSnsController extends Controller
         // Log::info('print_r($response, true) '.print_r($response['notificationType'], true));
         $responseMail = $response['mail'];
         Log::info('$responseMail[messageId] '.$responseMail['messageId']);  
-        $dataEviusMessage = [
-            'server_message_id' => $responseMail['messageId']
-        ];
-        $eviusMessageModel = new EviusMessage($dataEviusMessage);
-        $eviusMessageModel->save();
+        // $dataEviusMessage = [
+        //     'server_message_id' => $responseMail['messageId']
+        // ];
+        // $eviusMessageModel = new EviusMessage($dataEviusMessage);
+        // $eviusMessageModel->save();
         // Log::info('eventType '.json_encode($response)['eventType']);
         // Log::info('notificationType '.json_decode($response, true)['notificationType']);
         // Log::info('notificationType '.$response['notificationType']);
