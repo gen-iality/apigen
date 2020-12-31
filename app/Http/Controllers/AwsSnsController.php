bdffc5547ff937622cbc6d2472cdf070d3475223
emjvp_mocion <emilio.vargas@mocionsoft.com>
2020-12-31 10:13:02 -0500

    Log::info('print_r($request->getHeaders(), true) '.print_r($request->getHeaders(), true));

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -27,7 +27,7 @@ class AwsSnsController extends Controller
     {        
         
         $response = $request->json()->all();
-        Log::info('json_encode($request->getHeaders()) '.json_encode($request->getHeaders()));
+        Log::info('print_r($request->getHeaders(), true) '.print_r($request->getHeaders(), true));
         Log::info('$response '.json_encode($response));
         $responseMail = $response['mail'];                                
         // Log::info('$responseMail[destination] '. json_encode($responseMail['destination']));
