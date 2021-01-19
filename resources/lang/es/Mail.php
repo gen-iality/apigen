7ad8ae9c569510a33024d7dc417abfe6c14b7879
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-01-19 16:36:38 -0500

    Adding language at Event

--- resources/lang/es/Mail.php
+++ resources/lang/es/Mail.php
@@ -6,5 +6,7 @@ return [
     'ingresar_al_evento' => 'Ingresar al Evento AQUÍ',
     'recomendar_navegador' =>  'Se recomienda usar los navegadores Google Chrome, Mozilla Firefox para ingresar, algunas características pueden no estar disponibles en navegadores no soportados',    
     'alternativa_ingreso' => 'Si tiene problemas con el botón de ingreso abra el siguiente enlace',
-    'boton_ingreso' =>  'Click aquí'
+    'boton_ingreso' =>  'Click aquí',
+    'fecha' => 'Fecha',
+    'inscripcion_exitosa' => 'la inscripción ha sido exitosa al evento',
 ];
