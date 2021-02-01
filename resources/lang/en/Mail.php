e0a9c3b779248d1260929369e4fab26ffb56f893
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-02-01 13:57:36 -0500

    Adding English translation in the RSVP email

---
+++ resources/lang/en/Mail.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return [
+    'saludo' => 'Hello',
+    'ingresar_al_evento' => 'Enter the Event HERE',
+    'recomendar_navegador' =>  'It is recommended to use Google Chrome, Mozilla Firefox browsers for login, some features may not be available on unsupported browsers.',    
+    'alternativa_ingreso' => 'If you have problems with the login button open the following link',
+    'boton_ingreso' =>  'Click here',
+    'fecha' => 'Date',
+    'hora' => 'Hour',
+    'inscripcion_exitosa' => 'registration for the event has been successful',
+    'correo_soporte' => 'If you have any problems logging in to the platform or during the sessions, please do not hesitate to contact us at soporte@evius.co  '
+];
