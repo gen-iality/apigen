6a409d8905dd319d0c00a3cc87553acc826bb9f6
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-03-08 09:59:55 -0500

    Arreglando index de meetings para exportar en cvs

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -150,24 +150,22 @@ class MeetingsController extends Controller
         $documents = $this->database->collection($path)->documents();
 
         $count = 0;
-        $attendees = [];
         foreach($documents as $document){
             $count++;
             if ($document->exists()) {
                 $data = $document->data();
                 
-                // foreach($data['attendees'] as $attendee){
-                //     $a = Attendee::find($attendee);
-                //     $attendees[] = isset($a)?$a->_id:"-".",".isset($a)?$a['properties']['email']: "--";
-                //    //var_dump($a->_id);
+                $attendees = [];
+                foreach($data['attendees'] as $attendee){
+                    $a = Attendee::find($attendee);                    
+                    $attendees[] = isset($a)?$a->_id:"-".",".isset($a)?$a['properties']['email']: "--";
                   
-                // }
-                array_push($attendees ,$data['attendees']);
+                }
                 $time = new Carbon($data['timestamp_start']);
                 $time->setTimezone('America/Bogota');
-                // $attendees=implode(",", $attendees);
-
-                echo "{$document->id()}" . ', <br/>';
+                $attendees=implode(",", $attendees);
+                
+                echo "{$document->id()}, {$time->format('Y-m-d H:s a')}, {$attendees}, {$data['request_status']}<br/>";
                 //printf('Document data for document %s:' . PHP_EOL, $document->id());
                 //print_r($document->data());
                 //printf(PHP_EOL);
@@ -175,7 +173,6 @@ class MeetingsController extends Controller
                 printf('Document %s does not exist!' . PHP_EOL, $snapshot->id());
             }
         }
-        var_dump($attendees);
         return "total: {$count}";
 
 
