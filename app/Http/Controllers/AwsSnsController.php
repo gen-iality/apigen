b4b1cbfe46f018b8970d916926d8185a7b868ca4
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-04-14 13:05:13 -0500

    Agregando condicional para que no se repitan las peticiones de los correos entrgados

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -78,8 +78,10 @@ class AwsSnsController extends Controller
                 break;
                 case 'Delivery':               
                     // $total_delivered =count($total);
-                    $message->total_delivered = $message->total_delivered + 1;                   
-                    $message->save();
+                    if(isset($responseMail->headers)){
+                        $message->total_delivered = $message->total_delivered + 1;                   
+                        $message->save();
+                    }
                 break;
                 case 'Open':
                     // $total_opened =count($total);
