db01f74310d8ec3b48811fb506abead9db1c05c7
JKLopez <juan.lopez@mocionsoft.com>
2021-04-21 08:20:12 -0500

    volviendo a colocar el token

--- app/Attendee.php
+++ app/Attendee.php
@@ -21,7 +21,8 @@ class Attendee extends Models\Attendee
     protected $observables = ['saved', 'created', 'updated' , 'deleted'];
     protected static $unguarded = true;
     protected $fillable = ['account_id', 'event_id', 'state_id', "checkedin_at", "checked_in", "checked_in_date", "properties", "activities", "rol_id", "enrollment_activity", "ticket_title", "ticket_id", "registered_devices" ] ;
-    protected $with = ["user:uid,email,displayName,names","rol", 'state', "ticket"];
+    protected $with = ["user","rol", 'state', "ticket"];
+    //protected $with = ["user:uid,email,displayName,names","rol", 'state', "ticket"];
    //protected $visible = ['_id','names','email','properties','user','account_id','score'];
     //protected $with = ["user","rol", 'state', "ticket"];
 
