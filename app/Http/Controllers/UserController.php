606b1e093a9eb3e4eb09151cd0ee036b94757a5e
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-06-28 16:21:53 -0500

    Se agrega reireccionamiento al front evius.co/events

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -338,7 +338,7 @@ class UserController extends UserControllerWeb
             $url_final_params["error"] = $e->getMessage();
 
         } finally {
-            $destination = $request->has('destination') ? $request->input('destination') : config('app.front_url');
+            $destination = $request->has('destination') ? $request->input('destination') : config('app.front_url') . '/events';
             return redirect()->away($destination . "?" . http_build_query($url_final_params));
         }
 
