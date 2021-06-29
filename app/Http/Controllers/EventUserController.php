36c9ea23872e3968363bb534ed7fcb85b9071fd8
eviusco <apps@mocionsoft.com>
2021-06-29 20:56:46 +0000

    Se cambia plantilla de correo de registro

--- app/Http/Controllers/EventUserController.php
+++ app/Http/Controllers/EventUserController.php
@@ -416,20 +416,13 @@ class EventUserController extends Controller
         if ($noSendMail === 'true') {
             return $eventUser;
         }
-        if ($event->send_custom_email) {
             Mail::to($email)
                 ->queue(
                     //string $message, Event $event, $eventUser, string $image = null, $footer = null, string $subject = null)
                     new \App\Mail\InvitationMailSimple("", $event, $eventUser, $image, "", $event->name)
                 );
             return $eventUser;
-        }
-        Mail::to($email)
-            ->queue(
-                //string $message, Event $event, $eventUser, string $image = null, $footer = null, string $subject = null)
-                new \App\Mail\InvitationMail("", $event, $eventUser, $image, "", $event->name)
-            );
-        return $eventUser;
+      
 
     }
 
