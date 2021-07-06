0c937298953eb75bde60fd4c1e544aa07290b4fe
emjvp_mocion <emilio.vargas@mocionsoft.com>
2021-07-06 12:04:38 -0500

    comentando $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::months(6));

--- app/Http/Controllers/GoogleAnalitycsController.php
+++ app/Http/Controllers/GoogleAnalitycsController.php
@@ -17,7 +17,7 @@ class GoogleAnalitycsController extends Controller
         $json = $req->json()->all();      
         
         //retrieve visitors and pageview data for the current day and the last seven days
-        $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+        // $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7));
 
         //retrieve visitors and pageviews since the 6 months ago
         // $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::months(6));
