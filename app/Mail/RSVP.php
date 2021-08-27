805cbad676c66a95a468a83f16a6f4085d0e95cd
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-08-27 11:11:00 -0500

    Se ajusta para que no se cambie la contraseña cuando se envia un correo

--- app/Mail/RSVP.php
+++ app/Mail/RSVP.php
@@ -82,7 +82,7 @@ class RSVP extends Mailable implements ShouldQueue
 
         $organization_picture = !empty($event->styles["event_image"]) && strpos($event->styles["event_image"], 'htt') === 0 ? $event->styles["event_image"] : null;
 
-        $password = isset($eventUser["properties"]["password"]) ? $eventUser["properties"]["password"] : "mocion.2040";
+        $password = isset($eventUser["properties"]["password"]) ? $eventUser["properties"]["password"] : $email;
         $eventUser_name = isset($eventUser["properties"]["names"]) ? $eventUser["properties"]["names"] : $eventUser["properties"]["displayName"];
 
         // lets encrypt !
