916b8355842f3603e523fe9426c85eeda09202ce
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-10-20 10:09:13 -0500

    Agregando metodos crud de organizationusers con documentacion

--- app/OrganizationUser.php
+++ app/OrganizationUser.php
@@ -8,7 +8,12 @@ use Moloquent;
 class OrganizationUser extends Moloquent
 {
     protected $fillable = [
-        'userid', 'organization_id', 'user_properties', 'properties', 'account_id', 'rol_id'
+        'userid', 
+        'organization_id', 
+        'user_properties', 
+        'properties', 
+        'account_id', 
+        'rol_id'
     ];
     protected $with = ['user', 'organization'];
 
@@ -16,12 +21,14 @@ class OrganizationUser extends Moloquent
     {
         return $this->belongsTo('App\Account', 'account_id');
     }
+
     public function organization()
     {
         return $this->belongsTo('App\Organization', 'organization_id');
     }
-/*     public function rol()
-{
-return $this->belongsTo('App\Rol','rol_id');
-} */
+
+    public function rol()
+    {
+        return $this->belongsTo('App\Rol', 'rol_id');
+    }
 }
