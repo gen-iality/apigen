077354fa86fbb96606a25d1425cd9ed1e272d5a1
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-10-20 22:39:23 -0500

    Corrigiendoactualizacion de los usuarios registrados en una actividad y se crea el campo checked_in

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -579,9 +579,12 @@ class ActivitiesController extends Controller
         $ActivityAssistant = ActivityAssistant::updateOrCreate(
             [   
                 'activity_id' => $data['activity_id'] ,
-                'user_id'=> $data['user_id'],
+                'user_id'=> $data['user_id']                
+            ],
+            [
                 'event_id' => $data['event_id'],
-                'checkedin_at' => $date
+                'checkedin_at' => $date,
+                'checked_in' => true
             ]
         );      
 
