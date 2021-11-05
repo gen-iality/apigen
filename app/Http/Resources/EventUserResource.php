3a3899522bc4938fbf5dc995e944763a202150a2
Leonardo Caicedo <leonardo.caicedo@mocionsoft.com>
2021-11-05 11:53:57 -0500

    Testing: Ocultar password de properties en event_user

--- app/Http/Resources/EventUserResource.php
+++ app/Http/Resources/EventUserResource.php
@@ -14,6 +14,25 @@ class EventUserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            '_id' => $this->_id,
+            'state_id' => $this->state_id,
+            'checked_in' => $this->checked_in,
+            'rol_id' => $this->rol_id,
+            'properties' => [
+                'email' => $this->properties['email'],
+                'names' => $this->properties['names'],
+            ],
+            'event_id' => $this->event_id,
+            'account_id' => $this->account_id,
+            'private_reference_number' => $this->private_reference_number,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+            // 'user' => $this->user,
+            'rol' => $this->rol,
+            'state' => $this->state,
+            'ticket' => $this->ticket
+        ];
     }
 }
