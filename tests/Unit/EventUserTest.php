2eeffd9e6838b2c01520a7b3c3c515a0580a5c61
Leonardo Caicedo <leonardo.caicedo@mocionsoft.com>
2021-11-08 20:07:06 -0500

    Testing Refactorizacion: Password en event_users

--- tests/Unit/EventUserTest.php
+++ tests/Unit/EventUserTest.php
@@ -17,13 +17,13 @@ class EventUserTest extends TestCase
     public function test_hide_password_all_event_user()
     {
         $response = $this->get('api/events/602ecc7d52fc536415397962/eventusers'); // dummy url
-        $properties = $response['data'][2]['properties']; // select test resource
+        $properties = $response['data'][0]['properties']; // select test resource
         $result = empty($properties['password']) ? true : false;
         $this->assertTrue($result);
     }
     public function test_hide_password_event_user()
     {
-        $response = $this->get('api/events/602ecc7d52fc536415397962/eventusers/61829d5cc936090c981a0e73'); // dummy url
+        $response = $this->get('api/events/602ecc7d52fc536415397962/eventusers/6189c688cade7819e826e4d3'); // dummy url
         $properties = $response['properties'];
         $result = empty($properties['password']) ? true : false;
         $this->assertTrue($result);
