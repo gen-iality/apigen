142157780bbdc348aaf081e97f7ba46f3dbb0cbf
JulianaGarciaCo <julianageraldineco@gmail.com>
2021-12-13 09:34:35 -0500

    Agregando primera prueba automatizada para changeUserPassword

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,8 +13,11 @@ class ExampleTest extends TestCase
      * @return void
      */
     public function testBasicTest()
-    {
-        $response = $this->put('changeuserpassword' , ['email' => 'geraldine.garcia+1@mocionsoft.com']);
+    {   
+        $email = 'geraldine.garcia+2@mocionsoft.com';
+        $event_id = '61b3b7819a1e9c1e2c57812f';
+        
+        $response = $this->putJson('api/changeuserpassword' , ['email' => $email , 'event_id' => $event_id]);
 
         $response->assertStatus(200);
     }
