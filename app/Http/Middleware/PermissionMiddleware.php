65b43aa1d1bcd2a69dc0fcd2161a4e88c7a7cc53
JulianaGarciaCo <julianageraldineco@gmail.com>
2022-01-17 10:39:46 -0500

    Quitando validaciones del middlware de permisos mientras se configura correctamente

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -15,74 +15,77 @@ class PermissionMiddleware
 {
     public function handle($request, Closure $next, $permission)
     {   
-        //Usuario autenticado
-        $user = Auth::user();
+        // //Usuario autenticado
+        // $user = Auth::user();
         
-        if ($user  === null) {
-            throw new AuthenticationException("No token provided. Unauthenticated");
-        }    
+        // if ($user  === null) {
+        //     throw new AuthenticationException("No token provided. Unauthenticated");
+        // }    
 
-        //Validar parámetros de url
-        $urlParameter = $request->route();
+        // //Validar parámetros de url
+        // $urlParameter = $request->route();
 
-        //Separar los permisos para el endpoint
-        $permissions = is_array($permission)
-        ? $permission
-        : explode('|', $permission);
+        // //Separar los permisos para el endpoint
+        // $permissions = is_array($permission)
+        // ? $permission
+        // : explode('|', $permission);
         
-        //Se valida el rol del usuario, y el evento u organización que puede editar.               
-        $userRol = '';
+        // //Se valida el rol del usuario, y el evento u organización que puede editar.               
+        // $userRol = '';
 
-        if(Attendee::where('account_id' , $user->_id) !== null)
-        {
-            $userRol = Attendee::where('account_id' , $user->_id);                       
-        }else{
-            $userRol = ModelHasRole::where('model_id' , $user->_id);
-        }
+        // if(Attendee::where('account_id' , $user->_id) !== null)
+        // {
+        //     $userRol = Attendee::where('account_id' , $user->_id);                       
+        // }else{
+        //     $userRol = ModelHasRole::where('model_id' , $user->_id);
+        // }
 
 
-        switch ($urlParameter->parameterNames()[0]) {
-            case 'event':
-                $userRol = $userRol->where('event_id' ,$urlParameter->parameter('event'))->first(['rol_id', 'role_id', 'properties']);
-                break;
-            case 'organization':
-                $userRol = $userRol->where('organization_id' ,$urlParameter->parameter('organization'))->first(['rol_id', 'role_id']);              
-                break;
-        }        
-        if($userRol !== null)
-        {    
-            //Como el usuario existe se busca los permisos que tiene el rol del usuario.
-            $permissionsUser = Permission::where('role_ids', $userRol->rol_id)->get();                               
+        // switch ($urlParameter->parameterNames()[0]) {
+        //     case 'event':
+        //         $userRol = $userRol->where('event_id' ,$urlParameter->parameter('event'))->first(['rol_id', 'role_id', 'properties']);
+        //         break;
+        //     case 'organization':
+        //         $userRol = $userRol->where('organization_id' ,$urlParameter->parameter('organization'))->first(['rol_id', 'role_id']);              
+        //         break;
+        // }        
+        // if($userRol !== null)
+        // {    
+        //     //Como el usuario existe se busca los permisos que tiene el rol del usuario.
+        //     $permissionsUser = Permission::where('role_ids', $userRol->rol_id)->get();                               
             
-            foreach ($permissions as $permission) 
-            {   
-                foreach ($permissionsUser as $permissionUser) 
-                {   
-                    $permissionAccepted = $permissionUser::where('name' ,$permission)->first();    
+        //     foreach ($permissions as $permission) 
+        //     {   
+        //         foreach ($permissionsUser as $permissionUser) 
+        //         {   
+        //             $permissionAccepted = $permissionUser::where('name' ,$permission)->first();    
                                
-                    if($permissionAccepted !== null)
-                    {   
+        //             if($permissionAccepted !== null)
+        //             {   
                         
-                        return $next($request);
-                    }  
-                } 
-            }
+        //                 return $next($request);
+        //             }  
+        //         } 
+        //     }
 
-            //Esta segunda validación se hace para organizar mejor el código, ya que se separa  en roles_has_permissions la relación entre ambos
-            $permissionsRolUser = RolesPermissions::where('rol_id', $userRol->rol_id)->get();                                       
+        //     //Esta segunda validación se hace para organizar mejor el código, ya que se separa  en roles_has_permissions la relación entre ambos
+        //     $permissionsRolUser = RolesPermissions::where('rol_id', $userRol->rol_id)->get();                                       
             
-            foreach ($permissions as $permission) 
-            {   
-                foreach ($permissionsRolUser as $permissionUser) 
-                {                       
-                    if($permissionUser->permission->name === $permission )
-                    {   
-                        return $next($request);
-                    }   
-                } 
-            }
-        }
-        throw abort(401 , "You don't have permission for do this action.");  
+        //     foreach ($permissions as $permission) 
+        //     {   
+        //         foreach ($permissionsRolUser as $permissionUser) 
+        //         {                       
+        //             if($permissionUser->permission->name === $permission )
+        //             {   
+        //                 return $next($request);
+        //             }   
+        //         } 
+        //     }
+        // }
+        // throw abort(401 , "You don't have permission for do this action.");  
+
+        return $next($request);
+
          
     }
 }
