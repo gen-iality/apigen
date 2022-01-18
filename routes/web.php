782ca1ec3e6deb79a7c2aaecc9f931d8c201cf8a
Leonardo Caicedo <leonardo.caicedo@mocionsoft.com>
2022-01-18 12:09:27 -0500

    Obtener datos de rutas con docs

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,35 @@ Route::get('test', 'EventController@index');
 
 
 Route::post('testpush', 'SendContentController@sendPushNotification');
+
+Route::get('routes', function() { // OBTENER TODAS LAS RUTAS DISPONIBLES Y MODIFICARLAS
+$routeCollection = Route::getRoutes();
+$allRoutes = [];
+foreach ($routeCollection as $value) {
+    array_push($allRoutes, ['url' => $value->uri, 'method' => $value->methods[0]]);
+}
+$routesMod1 = [];
+foreach ($allRoutes as $route) {
+    array_push($routesMod1, ['url' => str_replace('{', ':', $route['url']), 'method' => $route['method']]);
+}
+$routesMod2 = [];
+foreach ($routesMod1 as $route) {
+    array_push($routesMod2, ['url' => str_replace('}', '', $route['url']), 'method' => $route['method']]);
+}
+// OBTENER TODAS LAS RUTAS DOCUMENTADAS
+$apidoc = json_decode( file_get_contents('../public/docs/collection.json'), true);
+$items = $apidoc['item'];
+$routeDocs = [];
+foreach ($items as $item) {
+    $requests = $item['item'];
+    foreach ($requests as $request) {
+      array_push($routeDocs, [ 'url' => $request['request']['url']['path'], 'method' => $request['request']['method'] ]);
+    }
+}
+// Ver rutas sin docs
+$routeWithoutDocs = [];
+foreach ($routesMod2 as $route) {
+  !in_array($route, $routeDocs) ? array_push($routeWithoutDocs, $route) : null ;
+}
+return ['No documentadas' => $routeWithoutDocs, 'Documentadas' => $routeDocs];
+});
