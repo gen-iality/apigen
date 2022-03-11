b0b5df155e874164acbfd7fa99e0656cef6a7521
Juliana García <geraldine.garcia@mocionsoft.com>
2022-03-11 12:06:38 -0500

    Desarrollo: roles etapa 2 /Tipo:bug /1.Se habilita route q que muestra la lista de roles

--- routes/api.php
+++ routes/api.php
@@ -364,7 +364,7 @@ Route::post("files/uploadbase/{name}", "FilesController@storeBaseImg");
 
 //Rol EndPoint
 // Route::get('events/{event}/rols', 'RolEventController@index');
-// Route::middleware('auth:token')->get('rols', 'RolController@index');
+Route::middleware('auth:token')->get('rols', 'RolController@index');
 // Route::post('rols', 'RolEventController@store');
 // Route::put('rols/{id}', 'RolEventController@update');
 // Route::get('rols/{id}', 'RolEventController@show');
