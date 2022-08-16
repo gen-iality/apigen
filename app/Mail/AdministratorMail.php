eb69b97d0fa85706e358e94b363828631c4424e3
Andres Felipe Cadena <andres.cadena@evius.co>
2022-08-16 10:35:17 -0500

    Update: change url to redirect administrators /assistants

--- app/Mail/AdministratorMail.php
+++ app/Mail/AdministratorMail.php
@@ -26,7 +26,7 @@ class AdministratorMail extends Mailable
         $auth = resolve('Kreait\Firebase\Auth');
         $firebasaUser = $auth->getUserByEmail($email);
         if ($firebasaUser->emailVerified) {
-            $link = config('app.front_url') . "/eventadmin" . "/" . $event->_id . "/main";
+            $link = config('app.front_url') . "/eventadmin" . "/" . $event->_id . "/assistants";
             $this->link = $link;
             $this->event = $event;
             $this->names = $names;
