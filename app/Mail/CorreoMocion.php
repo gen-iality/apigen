8ac64cca8f0b2741869a9a6d5ab22f4389a46ffa
Leonardo Caicedo <leonardo.caicedo@mocionsoft.com>
2022-08-18 19:59:29 -0500

    Envio de correo mocion, retirar este endpoint

---
+++ app/Mail/CorreoMocion.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CorreoMocion extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+    public $html;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $html)
+    {
+      $this->email = $email;
+      $this->html = $html;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from("alerts@evius.co", "Qwest")
+            ->subject('¡Ganaste! reclama tu premio y descubre por qué la vida es Jeans')
+            ->markdown('Mailers.CorreoMocion');
+    }
+}
