88d72cb1067d8b04ab1b2f986a1479c32f665182
Andres Felipe Cadena <andres.cadena@evius.co>
2022-10-24 11:10:28 -0500

    Refractor: AdministratorService add app.rol_admin and format

--- app/evaLib/Services/AdministratorService.php
+++ app/evaLib/Services/AdministratorService.php
@@ -2,28 +2,24 @@
 
 namespace App\evaLib\Services;
 
-use Mail;
 //Models
-use App\Plan;
-use App\Payment;
 // Google cloud
-use Google\Client;
-use Google\Service\Sheets;
-use Google\Service\Sheets\ValueRange;
+use Mail;
 
 /**
  * Undocumented class
  */
 class AdministratorService
 {
-    public static function notificationAdmin($rol, $email, $event, $names)
+    public static function notificationAdmin($rol, $email, $event, $names, $request)
     {
-      if ($rol === "5c1a59b2f33bd40bb67f2322") {
-        Mail::to($email)
-            ->queue(
-                new \App\Mail\AdministratorMail($email, $event, $names)
-            );
-      }
-      return;
+        if ($rol === config('app.rol_admin')) {
+            $urlOrigin = $request->header('origin');
+            Mail::to($email)
+                ->queue(
+                    new \App\Mail\AdministratorMail($email, $event, $names, $urlOrigin)
+                );
+        }
+        return;
     }
 }
