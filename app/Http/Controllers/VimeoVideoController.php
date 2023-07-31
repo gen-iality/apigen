5ec4837fcf40de65291a925f1b34fb0483ff316a
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-07-31 09:48:47 -0500

    Change: 1. Adapt the video upload code to vimeo

--- app/Http/Controllers/VimeoVideoController.php
+++ app/Http/Controllers/VimeoVideoController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Vimeo\Vimeo;
+//use Vimeo\Laravel\Facades\Vimeo;
 use Log;
 
 class VimeoVideoController extends Controller
 {
     //
-    protected $folder_id = "17085501"; // For the GEN.iality folder
+    protected $folder_id = "17085501"; // Evius
 
     private function uploadVideoToVimeo($file, $file_name, $file_description) {
         $client = new Vimeo(
@@ -63,7 +64,8 @@ class VimeoVideoController extends Controller
     }
 
 
-    public function videoStatus(Request $request, string $video_id = null) {
+    public function videoStatus(Request $request, string $video_id = null)
+    {
         // First, take the file sent
         $client = new Vimeo(
             "4f529fcb6e53b1d98128f422b0da074c4e5dcd3d",
@@ -71,59 +73,95 @@ class VimeoVideoController extends Controller
             "0deea97a5f2f0316ef84de0a33d83421",
         );
 
-        $video_id = $request->query('video_id');
+        $videoId = $request->query('video_id');
 
-        $uri = '/videos/'.$video_id.'?fields=uri,upload.status,transcode.status';
+        //$uri = '/videos/'.$video_id.'?fields=uri,upload.status,transcode.status';
 
-        $response = $client->request($uri);
+        $response = $client->request("/videos/$videoId");
+	return $response['body'];
         $body = $response['body'];
         return response()->json(['status' => json_encode( $body) ]);
+    }
 
-    }  
+    //public function uploadVideo(Request $request)
+    //{
+	//$this->validate($request, [
+	    //'video' => 'required|mimes:mp4,ogx,oga,ogv,ogg,webm'
+	//]);
 
-    public function uploadVideo(Request $request, string $field_name = null) {
-        // First, take the file sent
-        $field_name = 'file';
-        if (!$request->hasFile($field_name)) {
-            $statusCode = "400";
-            $message = "No file found in field '" . $field_name . "' to be uploded";
-            return response()->json(['error' => $message], $statusCode);
-        }
-        $file = $request->file($field_name);
-        //$files = is_array($files) ? $files : [$files];
+	//$video = $request->video;
+	//$nombreVideo = $video->getClientOriginalName();
 
-        // We need this the description or not, we don't care
-        $file_description = $request->query('description');
-        if (!isset($file_description)) $file_description = "Nuevo vídeo subido [fecha]";
+	//$uri = Vimeo::upload($video, [
+	    //'title' => $nombreVideo,
+	    //'description' => 'Description'
+	//]);
 
-        // For each video, we will upload and save the vimeo uri in an array
-        $all_good = true;
-        //$uri_list = [];
+	//return $uri;
+    //}
+
+    public function uploadVideo(Request $request, string $field_name = null)
+    {
+	$request->validate([
+	    'video' => 'required|mimes:mp4,ogx,oga,ogv,ogg,webm',
+	    'description' => 'string|max:500'
+	]);
+
+	$video = $request->video;
+	$videoName = $video->getClientOriginalName();
+	$videoDescription = isset($request->description) ?
+	    $request->description : 'New video';
+
+	try {
+	    $video = $this->uploadVideoToVimeo($video->getPathname(), $videoName, $videoDescription);
+	} catch(\Exception $error) {
+	    return response()->json(compact('error'), 400);
+	}
+
+	return response()->json(compact('video'));
+
+        //// First, take the file sent
+	//$field_name = 'file';
+	//if (!$request->hasFile($field_name)) {
+	    //$statusCode = "400";
+	    //$message = "No file found in field '" . $field_name . "' to be uploded";
+	    //return response()->json(['error' => $message], $statusCode);
+	//}
+	//$file = $request->file($field_name);
+	//$files = is_array($files) ? $files : [$files];
+
+	// We need this the description or not, we don't care
+	//$file_description = $request->query('description');
+	//if (!isset($file_description)) $file_description = "Nuevo vídeo subido [fecha]";
+
+	// For each video, we will upload and save the vimeo uri in an array
+	//$all_good = true;
+	//$uri_list = [];
 	//foreach ($files as $file) {
-            //name
-            $file_name = time();
-            if (is_object($file) && isset($file->getClientOriginalName)) {
-                $file_name .= "_" . preg_replace("/[^[:alnum:][:space:]]/u", '', $file->getClientOriginalName());
-            }
-            // If the video has no extension, then we set .mp4 just because
-            if (strpos($file_name, '.') == false) {
-                $file_name .= '.mp4';
-            }
-            Log::debug("will upload as ".$file_name);
+	    //name
+	    //$file_name = time();
+	    //if (is_object($file) && isset($file->getClientOriginalName)) {
+		//$file_name .= "_" . preg_replace("/[^[:alnum:][:space:]]/u", '', $file->getClientOriginalName());
+	    //}
+	    // If the video has no extension, then we set .mp4 just because
+	    //if (strpos($file_name, '.') == false) {
+		//$file_name .= '.mp4';
+	    //}
+	    //Log::debug("will upload as ".$file_name);
 
-            $video = $this->uploadVideoToVimeo($file->getPathname(), $file_name, $file_description);
+	    //$video = $this->uploadVideoToVimeo($video->getPathname(), $videoName, $file_description);
 
-            //if ($uri) {
-                //array_push($uri_list, $uri);
-            //} else {
-                //Log::error('Cannot upload this video: '.$file);
-                //$all_good = false;
-            //}
-        //}
+	    //if ($uri) {
+		//array_push($uri_list, $uri);
+	    //} else {
+		//Log::error('Cannot upload this video: '.$file);
+		//$all_good = false;
+	    //}
+	//}
 
-        if (!$all_good) {
-            return response()->json(['error' => 'Cannot upload the files and nobody knows the reason :c']);
-        }
-        return response()->json(compact('video'));
+	//if (!$all_good) {
+	    //return response()->json(['error' => 'Cannot upload the files and nobody knows the reason :c']);
+	//}
+	//return response()->json(compact('video'));
     }
 }
