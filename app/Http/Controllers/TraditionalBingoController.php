2fb7c04dab50a3854eff3a8637544cccd93f410a
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-08-22 11:26:50 -0500

    Change: regulation isn't mandatory

--- app/Http/Controllers/TraditionalBingoController.php
+++ app/Http/Controllers/TraditionalBingoController.php
@@ -12,11 +12,11 @@ class TraditionalBingoController extends Controller
 {
     // Valores bingo tradicional
     private const traditionalBingoValues = [
-	    'B' => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-	    'I' => [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
-	    'N' => [31,32,33,34,35,36,37,38,39,40,41,42,43,44,45],
-	    'G' => [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60],
-	    'O' => [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75],
+        'B' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        'I' => [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+        'N' => [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
+        'G' => [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+        'O' => [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75],
     ];
 
     /**
@@ -27,12 +27,12 @@ class TraditionalBingoController extends Controller
      */
     public function traditionalBingobyEvent(string $event_id)
     {
-      $traditionalBingo =  TraditionalBingo::where('event_id', $event_id)->first();
-      if(!$traditionalBingo) {
-	    return response()->json(['message' => 'Bingo not found'], 404);
-      }
+        $traditionalBingo =  TraditionalBingo::where('event_id', $event_id)->first();
+        if (!$traditionalBingo) {
+            return response()->json(['message' => 'Bingo not found'], 404);
+        }
 
-      return $traditionalBingo;
+        return $traditionalBingo;
     }
 
     /**
@@ -53,19 +53,19 @@ class TraditionalBingoController extends Controller
      */
     public function store(Request $request, $event)
     {
-	$request->validate([
-	    'name' => 'required|string|max:255'
-	]);
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
 
-	$data = $request->json()->all();
+        $data = $request->json()->all();
 
-	// Datos default para bingo tradicional
-	$data['bingo_values'] = self::traditionalBingoValues;
-	$data['event_id'] = $event;
+        // Datos default para bingo tradicional
+        $data['bingo_values'] = self::traditionalBingoValues;
+        $data['event_id'] = $event;
 
-	$traditionalBingo = TraditionalBingo::create($data);
+        $traditionalBingo = TraditionalBingo::create($data);
 
-	return response()->json(compact('traditionalBingo'), 201);
+        return response()->json(compact('traditionalBingo'), 201);
     }
 
     /**
@@ -88,16 +88,15 @@ class TraditionalBingoController extends Controller
      */
     public function update(Request $request, $event, TraditionalBingo $traditionalBingo)
     {
-	$request->validate([
-	    'name' => 'string|max:255',
-	    'regulation' => 'string'
-	]);
+        $request->validate([
+            'name' => 'string|max:255'
+        ]);
 
-	$data = $request->json()->all();
-	$traditionalBingo->fill($data);
-	$traditionalBingo->save();
+        $data = $request->json()->all();
+        $traditionalBingo->fill($data);
+        $traditionalBingo->save();
 
-	return response()->json(compact('traditionalBingo'), 200);
+        return response()->json(compact('traditionalBingo'), 200);
     }
 
     /**
@@ -108,17 +107,17 @@ class TraditionalBingoController extends Controller
      */
     public function destroy(Event $event, TraditionalBingo $traditionalBingo)
     {
-	// Eliminar todos los cartones de ese bingo
-	BingoCard::where('event_id', $event->_id)
-      	  ->where('tradicional_bingo_id', $traditionalBingo->_id)
-      	  ->delete();
+        // Eliminar todos los cartones de ese bingo
+        BingoCard::where('event_id', $event->_id)
+            ->where('tradicional_bingo_id', $traditionalBingo->_id)
+            ->delete();
 
-	$traditionalBingo->delete();
+        $traditionalBingo->delete();
 
         //Cambiar estado del bingo en el evento
         $event->bingo = null;
         $event->save();
 
-	return response()->json([], 204);
+        return response()->json([], 204);
     }
 }
