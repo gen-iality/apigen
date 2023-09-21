9450651cfa0723da6ce4078470ea789c7a77a9e6
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-09-21 12:08:43 -0500

    Hardcode: add colum Pais for evento Wella when export excel

--- resources/views/exports/burnedTickets.blade.php
+++ resources/views/exports/burnedTickets.blade.php
@@ -13,24 +13,34 @@
             <th><strong>Fecha de compra</strong></th>
             <th><strong>Email de compra</strong></th>
             <th><strong>Nombre de Comprador</strong></th>
+            @if ($burnedTickets[0]->event_id === '64c01263743610c1e50b6c76')
+                <th><strong>País</strong></th>
+            @endif
         </tr>
     </thead>
     <tbody>
         @foreach($burnedTickets as $burnedTicket)
-            <tr>
-                <td>{{ $burnedTicket->assigned_to['name'] }}</td>
-                <td>{{ $burnedTicket->assigned_to['email'] }}</td>
-                <td>{{ $burnedTicket->ticketCategory->name['es'] }}</td>
-                <td>{{ $burnedTicket->code }}</td>
-                <td>{{ $burnedTicket->state }}</td>
-                <td>${{ $burnedTicket->price_usd }}</td>
-                <td>${{ $burnedTicket->amount_in_cents / 100 }}</td>
-                <td>{{ $burnedTicket->assigned_to['document']['type_doc'] }}</td>
-                <td>{{ $burnedTicket->assigned_to['document']['value'] }}</td>
-                <td>{{ $burnedTicket->created_at }}</td>
-                <td>{{ $burnedTicket->billing['data']['transaction']['customer_email'] }}</td>
-                <td>{{ $burnedTicket->billing['data']['transaction']['customer_data']['full_name'] }}</td>
-            </tr>
+        <tr>
+            <td>{{ $burnedTicket->assigned_to['name'] }}</td>
+            <td>{{ $burnedTicket->assigned_to['email'] }}</td>
+            <td>{{ $burnedTicket->ticketCategory->name['es'] }}</td>
+            <td>{{ $burnedTicket->code }}</td>
+            <td>{{ $burnedTicket->state }}</td>
+            <td>${{ $burnedTicket->price_usd }}</td>
+            <td>${{ $burnedTicket->amount_in_cents / 100 }}</td>
+            <td>{{ $burnedTicket->assigned_to['document']['type_doc'] }}</td>
+            <td>{{ $burnedTicket->assigned_to['document']['value'] }}</td>
+            <td>{{ $burnedTicket->created_at }}</td>
+            <td>{{ $burnedTicket->billing['data']['transaction']['customer_email'] }}</td>
+            <td>{{ $burnedTicket->billing['data']['transaction']['customer_data']['full_name'] }}</td>
+            @if ($burnedTicket->event_id === '64c01263743610c1e50b6c76')
+                @if (isset( $burnedTicket->assigned_to['country'] ))
+                    <td>{{ $burnedTicket->assigned_to['country'] }}</td>
+                @else
+                    <td>{{''}}</td>
+                @endif
+            @endif
+        </tr>
         @endforeach
     </tbody>
 </table>
