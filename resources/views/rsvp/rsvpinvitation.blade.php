3e71403bf1d40ba0890d38fb4569be9fa920330b
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-09-22 10:57:19 -0500

    Hardcode: no send email for 65035e1e18f62b38c40ca4d4

--- resources/views/rsvp/rsvpinvitation.blade.php
+++ resources/views/rsvp/rsvpinvitation.blade.php
@@ -42,7 +42,7 @@
 <div style="text-align: center">
 	@if($event->type_event == "physicalEvent" && $event->_id != '65035e1e18f62b38c40ca4d4')
 		<img  src="{{$qr}}" />
-		@elseif($event->type_event == "onlineEvent" || $event->_id === '65035e1e18f62b38c40ca4d4'')
+		@elseif($event->type_event == "onlineEvent" || $event->_id === '65035e1e18f62b38c40ca4d4')
 			@component('mail::button', ['url' => $link , 'color' => 'evius'])
 				{{ __ ('Mail.enter_event')}}
 			@endcomponent
