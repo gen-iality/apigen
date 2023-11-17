4d1ea61093055dc53ca0c5245a8bc074d023912c
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-11-17 10:44:09 -0500

    feat: add filter to get free bingos

--- app/Http/Controllers/BingoCardController.php
+++ app/Http/Controllers/BingoCardController.php
@@ -19,41 +19,58 @@ class BingoCardController extends Controller
     /**
      * _index_: Get all bingo cards by bingo
      * @urlParam bingo required The id of the bingo to retrive all bingo cards.
-    */
+     */
     public function index(Request $request, Bingo $bingo)
     {
-	$numberItems = $request->query('numberItems') ? $request->query('numberItems'): 10;
-	$bingoCards = BingoCard::where('bingo_id', $bingo->_id)->paginate($numberItems);
+        $numberItems = $request->query('numberItems') ?
+            $request->query('numberItems') : 10;
 
-	return response()->json($bingoCards, 200);
+        $freeCards = $request->query('free_cards') === 'true' ?
+            true : false;
+
+        // Cartones libre
+        if ($freeCards) {
+            $bingoCards = BingoCard::where('bingo_id', $bingo->_id)
+                ->where('event_user_id', null)
+                ->paginate($numberItems);
+
+            return response()->json($bingoCards, 200);
+        }
+
+        // Cartones asignados
+        $bingoCards = BingoCard::where('bingo_id', $bingo->_id)
+            ->where('event_user_id', '!=', null)
+            ->paginate($numberItems);
+
+        return response()->json($bingoCards, 200);
     }
 
     /**
      * _show_: Get a event user with bingo card
      * @urlParam bingocard required The id of the bingo card to retrive this and the owner's data.
-    */
+     */
     public function show(string $code)
     {
         $bingo_card = BingoCard::where('code', $code)->first();
         $eventUser = Attendee::findOrFail($bingo_card->event_user_id);
-        return ["bingo_card" => $bingo_card, "name_owner"=> $eventUser["properties"]["names"]];
+        return ["bingo_card" => $bingo_card, "name_owner" => $eventUser["properties"]["names"]];
     }
 
     public function createBingoCardsWithoutAtteendes(Request $request, Bingo $bingo)
     {
-      $request->validate([
-	'qty_bingo_cards' => 'required|numeric'
-      ]);
+        $request->validate([
+            'qty_bingo_cards' => 'required|numeric'
+        ]);
 
-      $qtyBingoCards  = $request->json('qty_bingo_cards');
-      $bingoCardCreated = [];
+        $qtyBingoCards  = $request->json('qty_bingo_cards');
+        $bingoCardCreated = [];
 
-      for($i = 0; $i < $qtyBingoCards; $i++) {
-	$bingoCard = UserEventService::generateBingoCardForAttendee($bingo->event_id, $event_user_id=null);
-	array_push($bingoCardCreated, $bingoCard);
-      }
+        for ($i = 0; $i < $qtyBingoCards; $i++) {
+            $bingoCard = UserEventService::generateBingoCardForAttendee($bingo->event_id, $event_user_id = null);
+            array_push($bingoCardCreated, $bingoCard);
+        }
 
-      return response()->json($bingoCardCreated, 201);
+        return response()->json($bingoCardCreated, 201);
     }
 
     /**
@@ -61,7 +78,7 @@ class BingoCardController extends Controller
      * @urlParam bingocard required The id of the bingo card to find this.
      *
      * @return 204 No Content
-    */
+     */
 
     public function destroy(string $event, BingoCard $bingocard)
     {
@@ -71,8 +88,8 @@ class BingoCardController extends Controller
 
     //public function downloadBingoCard(BingoCard $bingocard)
     //{
-        //$pdf = PDF::loadview('BingoCard', compact('bingocard'));
+    //$pdf = PDF::loadview('BingoCard', compact('bingocard'));
 
-        //return $pdf->download('carton-bingo.pdf');
+    //return $pdf->download('carton-bingo.pdf');
     //}
 }
