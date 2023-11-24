3563eb8c2195caff4fe6b9044c4463bcf7f48cbb
Leonardo Caicedo <leonardo.caicedo@evius.co>
2023-11-24 10:49:31 -0500

    fix: no send word Usuario when retrive bingo card by code

--- app/Http/Controllers/BingoCardController.php
+++ app/Http/Controllers/BingoCardController.php
@@ -57,7 +57,7 @@ class BingoCardController extends Controller
         if (!$eventUser) {
             return response()->json([
                 "bingo_card" => $bingo_card,
-                "name_owner" => "Usuario"
+                "name_owner" => ""
             ]);
         }
 
